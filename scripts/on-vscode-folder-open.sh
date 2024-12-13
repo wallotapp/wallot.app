ef988e1e93ffaede91691c7aaf8763eba8b0c6ff
Kamar <kamarmack.home@gmail.com>
2024-12-13 09:38:56 +0000

    Remove unnecessary smoke test .env file script

--- scripts/on-vscode-folder-open.sh
+++ scripts/on-vscode-folder-open.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-## Open smoke test environment variables, if they are missing or empty
-([ ! -s smoke-tests/.env ] && code smoke-tests/.env || true);
-
 ## Open environment variables, if they are missing or empty (REST API)
 ([ ! -s expressjs-servers/rest-api/.env ] && code expressjs-servers/rest-api/.env || true);
 ([ ! -s expressjs-servers/rest-api/gmailApiServiceAccount.json ] && code expressjs-servers/rest-api/gmailApiServiceAccount.json || true);
