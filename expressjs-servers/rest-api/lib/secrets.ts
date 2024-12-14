280a325c89bd1c735235cc9f123176b454ea69c4
Kamar <kamarmack.home@gmail.com>
2024-12-14 11:12:40 +0000

    Add a alpacaBrokerApiClient helper; Refactor environment variables

--- expressjs-servers/rest-api/lib/secrets.ts
+++ expressjs-servers/rest-api/lib/secrets.ts
@@ -3,13 +3,14 @@ import { GeneralizedSecretData, getSecretData } from 'ergonomic-node';
 import { directoryPath } from './directoryPath.js';
 
 export type SecretData = GeneralizedSecretData & {
+	SECRET_CRED_ALPACA_BROKER_API_BASE_URL: string;
+	SECRET_CRED_ALPACA_BROKER_API_KEY: string;
+	SECRET_CRED_ALPACA_BROKER_API_SECRET: string;
 	SECRET_CRED_ALPHA_VANTAGE_API_KEY: string;
-	SECRET_CRED_ALPACA_API_KEY: string;
-	SECRET_CRED_ALPACA_API_SECRET: string;
 	SECRET_CRED_OPENAI_API_KEY: string;
 	SECRET_CRED_STRIPE_API_KEY: string;
-	SECRET_CRED_STRIPE_PREMIUM_LICENSE_PRODUCT_ID: string;
-	SECRET_CRED_STRIPE_PREMIUM_LICENSE_PRODUCT_MONTHLY_PRICE_ID: string;
+	SECRET_CRED_STRIPE_PRO_LICENSE_PRODUCT_ID: string;
+	SECRET_CRED_STRIPE_PRO_LICENSE_PRODUCT_MONTHLY_PRICE_ID: string;
 };
 
 const envPath = `${directoryPath}/../.env`;
