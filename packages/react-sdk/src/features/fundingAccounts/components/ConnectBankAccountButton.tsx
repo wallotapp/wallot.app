11e1d827bd3b36da9e3f7466b3dee00a0e3705eb
Kamar <kamarmack.home@gmail.com>
2024-12-14 08:07:44 +0000

    WIP #23: Add ACH relationship mutation to connect button

--- packages/react-sdk/src/features/fundingAccounts/components/ConnectBankAccountButton.tsx
+++ packages/react-sdk/src/features/fundingAccounts/components/ConnectBankAccountButton.tsx
@@ -1,14 +1,59 @@
 import { GeneralizedResponse } from 'ergonomic';
 import { Button } from 'ergonomic-react/src/components/ui/button';
 import { stripePromise } from 'ergonomic-react/src/lib/stripe';
+import { useToast } from 'ergonomic-react/src/components/ui/use-toast';
+import {
+	AlpacaAchRelationship,
+	StripeFinancialConnectionSession,
+} from '@wallot/js';
+import { useCreateAlpacaAchRelationshipsMutation } from '@wallot/react/src/features/fundingAccounts/hooks/useCreateAlpacaAchRelationshipsMutation';
 import { useCreateStripeFinancialConnectionSessionMutation } from '@wallot/react/src/features/fundingAccounts/hooks/useCreateStripeFinancialConnectionSessionMutation';
-import { StripeFinancialConnectionSession } from '@wallot/js';
+import { useState } from 'react';
+
+const defaultErrorMessage =
+	'We ran into a problem connecting your bank account';
 
 export const ConnectBankAccountButton = () => {
+	// Toaster
+	const { toast } = useToast();
+
+	// Alpaca ACH relationships
+	const [alpacaAchRelationships, setAlpacaAchRelationships] = useState<
+		AlpacaAchRelationship[]
+	>([]);
+
+	const {
+		mutate: createAlpacaAchRelationships,
+		isLoading: isCreateAlpacaAchRelationshipsRunning,
+	} = useCreateAlpacaAchRelationshipsMutation({
+		onError: ({ errors }: GeneralizedResponse) => {
+			const error = errors[0]?.error.message || defaultErrorMessage;
+			toast({
+				title: 'Error',
+				description: error,
+			});
+		},
+		onSuccess: ({ data }: GeneralizedResponse<AlpacaAchRelationship>) => {
+			setAlpacaAchRelationships(() => data);
+			toast({
+				title: 'Success',
+				description: 'Bank account connected',
+			});
+			console.log('Alpaca ACH relationships created:', data);
+		},
+	});
+
 	const {
 		mutate: createStripeFinancialConnectionSession,
 		isLoading: isCreateStripeFinancialConnectionSessionRunning,
 	} = useCreateStripeFinancialConnectionSessionMutation({
+		onError: ({ errors }: GeneralizedResponse) => {
+			const error = errors[0]?.error.message || defaultErrorMessage;
+			toast({
+				title: 'Error',
+				description: error,
+			});
+		},
 		onSuccess: async ({
 			data,
 		}: GeneralizedResponse<StripeFinancialConnectionSession>) => {
@@ -22,21 +67,40 @@ export const ConnectBankAccountButton = () => {
 			if (!financialConnectionsSession) {
 				throw new Error('Failed to collect financial connections accounts');
 			}
+			console.log(
+				'Financial connections session:',
+				financialConnectionsSession,
+			);
 			const { accounts } = financialConnectionsSession;
 			const accountIds: string[] = accounts.map((account) => account.id);
-			// Pass the list of account IDs to the backend to establish the Alpaca ACH relationship
-			console.log(accountIds);
+			createAlpacaAchRelationships({
+				stripe_financial_account_ids: accountIds,
+			});
 		},
 	});
+
+	const operationState =
+		isCreateAlpacaAchRelationshipsRunning ||
+		isCreateStripeFinancialConnectionSessionRunning
+			? 'running'
+			: 'idle';
+	const isRunning = operationState === 'running';
+
+	if (alpacaAchRelationships.length) {
+		return (
+			<Button disabled>
+				{alpacaAchRelationships.length} bank account
+				{alpacaAchRelationships.length > 1 ? 's' : ''} connected
+			</Button>
+		);
+	}
 
 	return (
 		<Button
 			onClick={() => createStripeFinancialConnectionSession({})}
-			disabled={isCreateStripeFinancialConnectionSessionRunning}
+			disabled={isRunning}
 		>
-			{isCreateStripeFinancialConnectionSessionRunning
-				? 'Connecting...'
-				: 'Connect your bank account'}
+			{isRunning ? 'Connecting...' : 'Connect your bank account'}
 		</Button>
 	);
 };
