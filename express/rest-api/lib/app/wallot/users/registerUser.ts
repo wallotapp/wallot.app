2e0a9959f47d1b9bd2ec35b4cdec5511b372ce7d
Kamar <kamarmack.home@gmail.com>
2024-12-16 12:20:04 +0000

    Finish the registerUser function

--- express/rest-api/lib/app/wallot/users/registerUser.ts
+++ express/rest-api/lib/app/wallot/users/registerUser.ts
@@ -7,48 +7,153 @@ import {
 	equityAccountsApi,
 	licensesApi,
 	ordersApi,
-	paymentMethodsApi,
+	getHomeWebAppRoute,
+	getFirestoreCollectionPath,
 } from '@wallot/js';
-import { auth } from '../../../services.js';
+import { auth, db } from '../../../services.js';
+import { siteOriginByTarget } from '../../../variables.js';
+import { createStripeCustomer } from '../../stripe/customers/createStripeCustomer.js';
 
 export const registerUser = async ({
 	email,
 	password,
 	username,
 }: RegisterUserParams): Promise<RegisterUserResponse> => {
-	// Construct Firestore document IDs in advance
-	const stripeCustomerId = stripeCustomersApi.generateId();
-	stripeCustomerId;
-	const userId = usersApi.generateId();
-	userId;
-	const authCredentialId = authCredentialsApi.generateId();
-	authCredentialId;
-	const equityAccountId = equityAccountsApi.generateId();
-	equityAccountId;
-	const licenseId = licensesApi.generateId();
-	licenseId;
-	const orderId = ordersApi.generateId();
-	orderId;
-	const paymentMethodId = paymentMethodsApi.generateId();
-	paymentMethodId;
+	// Initialize Firestore collection names
+	const stripeCustomerCollectionName = getFirestoreCollectionPath(
+		stripeCustomersApi.apiResourceCollectionId,
+	);
+	const userCollectionName = getFirestoreCollectionPath(
+		usersApi.apiResourceCollectionId,
+	);
+	const authCredentialCollectionName = getFirestoreCollectionPath(
+		authCredentialsApi.apiResourceCollectionId,
+	);
+	const equityAccountCollectionName = getFirestoreCollectionPath(
+		equityAccountsApi.apiResourceCollectionId,
+	);
+	const licenseCollectionName = getFirestoreCollectionPath(
+		licensesApi.apiResourceCollectionId,
+	);
+	const orderCollectionName = getFirestoreCollectionPath(
+		ordersApi.apiResourceCollectionId,
+	);
+
+	// Initialize Firestore document IDs
+	const stripeCustomerDocId = stripeCustomersApi.generateId();
+	const userDocId = usersApi.generateId();
+	const authCredentialDocId = authCredentialsApi.generateId();
+	const equityAccountDocId = equityAccountsApi.generateId();
+	const licenseDocId = licensesApi.generateId();
+	const orderDocId = ordersApi.generateId();
+
+	// Initialize a Firestore batch transaction
+	const batch = db.batch();
 
 	// Register a Firebase User
-	// Create a Stripe Customer
-	// Create a STRIPE_CUSTOMER Firestore document
-	// Create a USER Firestore document
-	// Create an AUTH_CREDENTIAL Firestore document
-	// Create an EQUITY_ACCOUNT Firestore document
-	// Create a LICENSE Firestore document
-	// Create an ORDER Firestore document
-	// Create a PAYMENT_METHOD Firestore document
-	// Create a Firebase custom token (JWT)
-	// Construct the redirect URL using custom token
+	const firebaseUser = await auth.createUser({
+		email,
+		password,
+		uid: authCredentialDocId,
+	});
 
-	email;
-	password;
-	username;
-	const firebaseUser = { uid: 'TODO' };
+	// Create a Stripe Customer
+	const stripeCustomer = await createStripeCustomer({
+		email,
+		metadata: { _id: stripeCustomerDocId, user_id: userDocId },
+		username,
+	});
+
+	// Create a STRIPE_CUSTOMER Firestore document
+	const stripeCustomerDoc = stripeCustomersApi.mergeCreateParams({
+		createParams: {
+			_id: stripeCustomerDocId,
+			category: 'default',
+			name: '',
+			stripe_id: stripeCustomer.id,
+		},
+	});
+	batch.set(
+		db.collection(stripeCustomerCollectionName).doc(stripeCustomerDocId),
+		stripeCustomerDoc,
+	);
+
+	// Create a USER Firestore document
+	const userDoc = usersApi.mergeCreateParams({
+		createParams: {
+			_id: userDocId,
+			category: 'default',
+			name: '',
+			stripe_customer: stripeCustomerDocId,
+			username,
+		},
+	});
+	batch.set(db.collection(userCollectionName).doc(userDocId), userDoc);
+
+	// Create an AUTH_CREDENTIAL Firestore document
+	const authCredentialDoc = authCredentialsApi.mergeCreateParams({
+		createParams: {
+			_id: authCredentialDocId,
+			category: 'default',
+			emails: [email],
+			name: '',
+			user: userDocId,
+		},
+	});
+	batch.set(
+		db.collection(authCredentialCollectionName).doc(authCredentialDocId),
+		authCredentialDoc,
+	);
+
+	// Create an EQUITY_ACCOUNT Firestore document
+	const equityAccountDoc = equityAccountsApi.mergeCreateParams({
+		createParams: {
+			_id: equityAccountDocId,
+			category: 'default',
+			name: '',
+			user: userDocId,
+		},
+	});
+	batch.set(
+		db.collection(equityAccountCollectionName).doc(equityAccountDocId),
+		equityAccountDoc,
+	);
+
+	// Create a LICENSE Firestore document
+	const licenseDoc = licensesApi.mergeCreateParams({
+		createParams: {
+			_id: licenseDocId,
+			category: 'default',
+			name: '',
+			user: userDocId,
+		},
+	});
+	batch.set(db.collection(licenseCollectionName).doc(licenseDocId), licenseDoc);
+
+	// Create an ORDER Firestore document
+	const orderDoc = ordersApi.mergeCreateParams({
+		createParams: {
+			_id: orderDocId,
+			category: 'default',
+			name: '',
+			user: userDocId,
+		},
+	});
+	batch.set(db.collection(orderCollectionName).doc(orderDocId), orderDoc);
+
+	// Commit the batch transaction
+	await batch.commit();
+
+	// Create a Firebase custom token (JWT)
 	const customToken = await auth.createCustomToken(firebaseUser.uid);
 
-	return { custom_token: customToken, redirect_url: 'TODO' };
+	// Construct the redirect URL using custom token
+	const redirectUrl = getHomeWebAppRoute({
+		includeOrigin: true,
+		origin: siteOriginByTarget.HOME_WEB_APP,
+		queryParams: { client_token: customToken },
+		routeStaticId: 'HOME_WEB_APP__/GET_STARTED',
+	});
+
+	return { custom_token: customToken, redirect_url: redirectUrl };
 };
