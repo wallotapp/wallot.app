91528d53a2f951b25d06d8127ca401365bdbc24b
Kamar <kamarmack.home@gmail.com>
2024-12-16 14:02:14 +0000

    Initialize a locateCompatibleParameters function

---
+++ express/rest-api/lib/app/wallot/parameters/locateCompatibleParameters.ts
@@ -0,0 +1,16 @@
+import { OnboardUserParams, Parameter } from '@wallot/js';
+
+export const locateCompatibleParameters = async ({
+	age_range,
+	capital_level,
+	investing_goals,
+	risk_level,
+}: OnboardUserParams): Promise<Parameter[]> => {
+	age_range;
+	capital_level;
+	investing_goals;
+	risk_level;
+	// Wait 1 second
+	await new Promise((resolve) => setTimeout(resolve, 2500));
+	return [];
+};
