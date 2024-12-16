b0489365e3e87778158996013c855ac68dcedbc7
Kamar <kamarmack.home@gmail.com>
2024-12-16 14:02:29 +0000

    Initialize a createRecommendationForUser function

---
+++ express/rest-api/lib/app/wallot/recommendations/createRecommendationForUser.ts
@@ -0,0 +1,15 @@
+import { OnboardUserParams, Parameter, Recommendation } from '@wallot/js';
+
+export const createRecommendationForUser = async (
+	{ age_range, capital_level, investing_goals, risk_level }: OnboardUserParams,
+	compatibleParameters: Parameter[],
+): Promise<Recommendation> => {
+	age_range;
+	capital_level;
+	investing_goals;
+	risk_level;
+	compatibleParameters;
+	// Wait 1 second
+	await new Promise((resolve) => setTimeout(resolve, 2500));
+	throw new Error('Not implemented');
+};
