4c6ecc10bb161d15934867111af010e45d6b2e75
Kamar <kamarmack.home@gmail.com>
2024-12-17 03:51:58 +0000

    Bump ergonomic-node; Install PubSub library

--- functions/pubsub/lib/index.ts
+++ functions/pubsub/lib/index.ts
@@ -2,6 +2,7 @@ import {
 	type CloudEvent,
 	// eslint-disable-next-line import/no-unresolved
 } from 'firebase-functions/v2';
+	// eslint-disable-next-line import/no-unresolved
 import { MessagePublishedData } from 'firebase-functions/v2/pubsub';
 import { firebaseFunctions } from 'ergonomic-node';
 
