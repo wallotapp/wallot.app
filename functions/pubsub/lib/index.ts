370d4e09431752ab11a75f10d7ad77e80be8603b
Kamar <kamarmack.home@gmail.com>
2024-12-17 03:12:27 +0000

    finish contrived examples of function topics

--- functions/pubsub/lib/index.ts
+++ functions/pubsub/lib/index.ts
@@ -2,14 +2,14 @@ import * as firebaseFunctions from 'firebase-functions';
 
 /**
  * Cloud Function for handling image uploads
- * 
+ *
  * This function is triggered by a Pub/Sub message published to the 'image-uploads' topic.
- * 
+ *
  * @example
  * ```typescript
  * import { PubSub } from '@google-cloud/pubsub';
  * const pubsub = new PubSub();
- * 
+ *
  * async function processImageAsynchronously(imagePath: string) {
  * 	const topic = pubsub.topic('image-uploads');
  * 	await topic.publishMessage({
@@ -17,21 +17,21 @@ import * as firebaseFunctions from 'firebase-functions';
  * 	});
  * 	console.log('Published image upload event.');
  * }
- * 
+ *
  * processImageAsynchronously('path/to/image.jpg').catch(console.error);
  * ```
-*/
+ */
 export const processImage = firebaseFunctions.pubsub
-  .topic('image-uploads')
-  .onPublish(async (message) => {
-    console.log('Processing image upload...');
-    const data = message.json as { imagePath: string };
-    await resizeAndValidateImage(data.imagePath); // Custom image processing logic
-    console.log('Image processing completed.');
-  });
+	.topic('image-uploads')
+	.onPublish(async (message) => {
+		console.log('Processing image upload...');
+		const data = message.json as { imagePath: string };
+		await resizeAndValidateImage(data.imagePath); // Custom image processing logic
+		console.log('Image processing completed.');
+	});
 
 async function resizeAndValidateImage(imagePath: string) {
 	// Example logic for resizing and validating images
-  await new Promise((resolve) => setTimeout(resolve, 2500));
-  console.log(`Resizing and validating image at: ${imagePath}`);
+	await new Promise((resolve) => setTimeout(resolve, 2500));
+	console.log(`Resizing and validating image at: ${imagePath}`);
 }
--- functions/triggers/lib/index.ts
+++ functions/triggers/lib/index.ts
@@ -1,6 +1,23 @@
+import {
+	type QueryDocumentSnapshot,
+	type FirestoreEvent,
+	// eslint-disable-next-line import/no-unresolved
+} from 'firebase-functions/v2/firestore';
 import { firebaseFunctions } from 'ergonomic-node';
 
-export const triggers = firebaseFunctions.https.onRequest(async (_req, res) => {
+export const onUserCreated = firebaseFunctions.firestore.onDocumentCreated(
+	'users/{userId}',
+	handleUserCreated,
+);
+
+async function handleUserCreated(
+	event: FirestoreEvent<QueryDocumentSnapshot | undefined, { userId: string }>,
+) {
+	const snapshot = event.data;
+	if (!snapshot) {
+		throw new Error('Document does not exist.');
+	}
+	const user = snapshot.data() as { name: string; email: string };
 	await new Promise((resolve) => setTimeout(resolve, 2500));
-	res.send('Hello from triggers!');
-});
+	console.log('New user created:', user);
+}
