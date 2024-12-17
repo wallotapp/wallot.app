f574917bf856ecd3a8f555f7000efed9c34a90e9
Kamar <kamarmack.home@gmail.com>
2024-12-17 13:50:21 +0000

    Add Firestore dependency to createRouterFunction and retrieve user ID from AUTH_CREDENTIAL document

--- packages/node-sdk/createRouterFunction.ts
+++ packages/node-sdk/createRouterFunction.ts
@@ -3,25 +3,29 @@ import {
 	type Auth,
 	type DecodedIdToken as FirebaseUser,
 } from 'firebase-admin/auth';
+// eslint-disable-next-line import/no-unresolved
+import { type Firestore, } from 'firebase-admin/firestore';
 import {
 	GeneralizedResLocals,
 	getAuthHeaderBearerToken,
 	isResLocalsJsonError,
 } from 'ergonomic-node';
-import { handleRouterFunctionError } from './handleRouterFunctionError.js';
 import { getGeneralizedError } from 'ergonomic';
+import { handleRouterFunctionError } from './handleRouterFunctionError.js';
+import { AuthCredential, authCredentialsApi } from '@wallot/js'
 
 /**
  * Creates an Express router function that handles asynchronous operations.
  */
 export const createRouterFunction =
-	(auth: Auth) =>
+	(auth: Auth, db: Firestore) =>
 	<TParams, TResponseBody, TRequestBody, TQuery>(
 		fn: (
 			body: TRequestBody,
 			params: TParams,
 			query: TQuery,
 			firebaseUser: FirebaseUser | null,
+			userId: string | null,
 		) => Promise<TResponseBody>,
 		options: { requiresAuth: boolean } = { requiresAuth: true },
 	) => {
@@ -56,6 +60,23 @@ export const createRouterFunction =
 							}
 						}
 
+						// Query the AUTH_CREDENTIAL document from Firestore using the db/{authCredentialsApi.collectionId}/{uid}
+						// Get the `user` property from the AUTH_CREDENTIAL document which is a foreign key to the `users` collection
+						let userId: string | undefined;
+						if (firebaseUser) {
+							const authCredentialDoc = await db
+								.collection(authCredentialsApi.collectionId)
+								.doc(firebaseUser.uid)
+								.get();
+							if (authCredentialDoc.exists) {
+								const authCredentialData = authCredentialDoc.data() as AuthCredential;
+								if (authCredentialData) {
+									userId = authCredentialData.user;
+								}
+							}
+						}
+						
+
 						// Extract the payload from the request body
 						const body = req.body;
 
@@ -71,6 +92,7 @@ export const createRouterFunction =
 							params,
 							query,
 							firebaseUser ?? null,
+							userId ?? null
 						);
 
 						// Proceed to the next middleware
