a834f689a3ce75d104cc718b50fa3411a169ba38
Kamar <kamarmack.home@gmail.com>
2024-12-18 10:45:34 +0000

    Add useRegisterUserMutation hook for user registration

---
+++ packages/react-sdk/src/features/users/hooks/useRegisterUserMutation.tsx
@@ -0,0 +1,26 @@
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import { GeneralizedError } from 'ergonomic';
+import { RegisterUserParams, RegisterUserResponse } from '@wallot/js';
+import { registerUser } from '@wallot/react/src/features/users/api/registerUser';
+
+export const useRegisterUserMutation = (
+	options?: UseMutationOptions<
+		RegisterUserResponse,
+		GeneralizedError,
+		RegisterUserParams
+	>,
+) => {
+	return useMutation<
+		RegisterUserResponse,
+		GeneralizedError,
+		RegisterUserParams
+	>((params: RegisterUserParams) => registerUser(params), {
+		onError: (error: GeneralizedError) => {
+			console.error('registerUser operation failed:', error);
+		},
+		onSuccess: (data: RegisterUserResponse) => {
+			console.log('registerUser operation successful', data);
+		},
+		...(options ?? {}),
+	});
+};
--- packages/react-sdk/src/features/users/index.tsx
+++ packages/react-sdk/src/features/users/index.tsx
@@ -1,4 +1,5 @@
 export * from '@wallot/react/src/features/users/hooks/useCreateUserMutation';
 export * from '@wallot/react/src/features/users/hooks/useQueryUserPage';
+export * from '@wallot/react/src/features/users/hooks/useRegisterUserMutation';
 export * from '@wallot/react/src/features/users/hooks/useUpdateUserMutation';
 export * from '@wallot/react/src/features/users/types/UserReactTypes';
