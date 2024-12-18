743833c06560d42143dd2809ebacd7e5be62a8ef
Kamar <kamarmack.home@gmail.com>
2024-12-18 11:12:10 +0000

    add a task id example

--- functions/cloud-tasks/lib/example.ts
+++ functions/cloud-tasks/lib/example.ts
@@ -7,6 +7,7 @@ import {
 import { getFunctions } from 'firebase-admin/functions';
 import { secrets } from './secrets.js';
 import { directoryPath } from './directoryPath.js';
+import { v4 } from 'uuid';
 const serviceAccountPath = `${directoryPath}/../gmailApiServiceAccount.json`;
 
 const scheduleAlpacaAccountStatusChecks = async (userId: string) => {
@@ -18,19 +19,23 @@ const scheduleAlpacaAccountStatusChecks = async (userId: string) => {
 		functionName: 'checkAlpacaAccountStatus',
 		serviceAccountPath,
 	});
+	const taskId = v4();
 	await queue.enqueue(
 		{ user_id: userId },
 		{
-			// How long to wait before the task is first delivered to the target service after it has been enqueued
-			// Suppose you enqueue a task at 10:00 AM with scheduleDelaySeconds: 3600 (which is one hour).
-			// The task won’t even be sent to your function until 11:00 AM
-			scheduleDelaySeconds: 0,
 			// Maximum amount of time that the target is allowed to run in response to the dispatched task
 			// Let’s say at 11:00 AM, Cloud Tasks dispatches the task to your function, and dispatchDeadlineSeconds: 300 (5 minutes) is set
 			// Your function must complete within 5 minutes.
 			// If it finishes at 11:04 AM, everything is fine.
 			// If it’s still running at 11:05 AM, Cloud Tasks will assume the attempt failed and will initiate a retry if configured.
 			dispatchDeadlineSeconds: 60 * 5, // <= 5 minutes
+			// The ID of the task. This is useful for ensuring that the task is only enqueued 
+			// once and for retrieving the task later.
+			id: taskId,
+			// How long to wait before the task is first delivered to the target service after it has been enqueued
+			// Suppose you enqueue a task at 10:00 AM with scheduleDelaySeconds: 3600 (which is one hour).
+			// The task won’t even be sent to your function until 11:00 AM
+			scheduleDelaySeconds: 0,
 			uri: targetUri,
 		},
 	);
