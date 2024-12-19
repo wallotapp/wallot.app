8fce1505c2caa6d10995c545c1506a63914c9792
Kamar <kamarmack.home@gmail.com>
2024-12-19 08:32:49 +0000

    Refactor getHomeWebAppRoute to consolidate route handling and improve client token management

--- packages/javascript-sdk/utils/getHomeWebAppRoute.ts
+++ packages/javascript-sdk/utils/getHomeWebAppRoute.ts
@@ -18,62 +18,51 @@ export const getHomeWebAppRoute = <T extends HomeWebAppRouteStaticId>(
 		console.error('Origin is required');
 		return '/';
 	}
-	if (routeStaticId === 'HOME_WEB_APP__/ASSETS') {
-		const queryParams =
-			options.queryParams as HomeWebAppRouteQueryParams['HOME_WEB_APP__/ASSETS'];
-		const clientToken = queryParams.client_token;
-		const clientTokenQuery = clientToken ? `client_token=${clientToken}` : '';
-		const queries = [clientTokenQuery].filter(Boolean);
-		const path = `/assets${queries.length ? `?${queries.join('&')}` : ''}`;
+
+	const clientToken = options.queryParams.client_token;
+	const clientTokenQuery = clientToken ? `client_token=${clientToken}` : '';
+	const queries = [clientTokenQuery].filter(Boolean);
+
+	if (
+		routeStaticId === 'HOME_WEB_APP__/ASSETS' ||
+		routeStaticId === 'HOME_WEB_APP__/INDEX' ||
+		routeStaticId === 'HOME_WEB_APP__/GET_STARTED'
+	) {
+		const path = `${
+			routeStaticId === 'HOME_WEB_APP__/ASSETS'
+				? '/assets'
+				: routeStaticId === 'HOME_WEB_APP__/INDEX'
+				? '/'
+				: '/get-started'
+		}${queries.length ? `?${queries.join('&')}` : ''}`;
 		if (includeOrigin) return `${origin as string}${path}`;
 		return path;
 	}
-	if (routeStaticId === 'HOME_WEB_APP__/INDEX') {
-		const queryParams =
-			options.queryParams as HomeWebAppRouteQueryParams['HOME_WEB_APP__/INDEX'];
-		const clientToken = queryParams.client_token;
-		const clientTokenQuery = clientToken ? `client_token=${clientToken}` : '';
-		const queries = [clientTokenQuery].filter(Boolean);
-		const path = `/${queries.length ? `?${queries.join('&')}` : ''}`;
-		if (includeOrigin) return `${origin as string}${path}`;
-		return path;
-	}
-	if (routeStaticId === 'HOME_WEB_APP__/GET_STARTED') {
-		const queryParams =
-			options.queryParams as HomeWebAppRouteQueryParams['HOME_WEB_APP__/GET_STARTED'];
-		const clientToken = queryParams.client_token;
-		const clientTokenQuery = clientToken ? `client_token=${clientToken}` : '';
-		const queries = [clientTokenQuery].filter(Boolean);
-		const path = `/get-started${queries.length ? `?${queries.join('&')}` : ''}`;
-		if (includeOrigin) return `${origin as string}${path}`;
-		return path;
-	}
+
 	if (
 		routeStaticId === 'HOME_WEB_APP__/ORDERS/[ORDER_ID]/ASSETS' ||
 		routeStaticId === 'HOME_WEB_APP__/ORDERS/[ORDER_ID]/CART' ||
 		routeStaticId === 'HOME_WEB_APP__/ORDERS/[ORDER_ID]/CHECKOUT'
 	) {
-		const queryParams = options.queryParams as
+		const orderRouteQueryParams = options.queryParams as
 			| HomeWebAppRouteQueryParams['HOME_WEB_APP__/ORDERS/[ORDER_ID]/ASSETS']
 			| HomeWebAppRouteQueryParams['HOME_WEB_APP__/ORDERS/[ORDER_ID]/CART']
 			| HomeWebAppRouteQueryParams['HOME_WEB_APP__/ORDERS/[ORDER_ID]/CHECKOUT'];
-		const orderId = queryParams.order_id;
+		const orderId = orderRouteQueryParams.order_id;
 		if (!orderId) {
 			console.error('order_id is required');
 			return '/';
 		}
-		const clientToken = queryParams.client_token;
-		const clientTokenQuery = clientToken ? `client_token=${clientToken}` : '';
-		const queries = [clientTokenQuery].filter(Boolean);
-		const path = `/orders/${orderId}/${
+		const path = `/orders/${orderId}${
 			routeStaticId === 'HOME_WEB_APP__/ORDERS/[ORDER_ID]/ASSETS'
-				? 'assets'
+				? '/assets'
 				: routeStaticId === 'HOME_WEB_APP__/ORDERS/[ORDER_ID]/CART'
-				? 'cart'
-				: 'checkout'
+				? '/cart'
+				: '/checkout'
 		}${queries.length ? `?${queries.join('&')}` : ''}`;
 		if (includeOrigin) return `${origin as string}${path}`;
 		return path;
 	}
+
 	throw new Error('Invalid routeStaticId');
 };
