e0b7e716a7e0b7e21597ca31c6d1f96420ad97cd
Kamar <kamarmack.home@gmail.com>
2024-12-19 07:12:16 +0000

    Update logout page to redirect to SSO login route using dynamic routing

--- sites/sso-web-app/pages/logout.tsx
+++ sites/sso-web-app/pages/logout.tsx
@@ -4,6 +4,7 @@ import { signOut } from 'firebase/auth';
 import { firebaseAuthInstance as auth } from 'ergonomic-react/src/lib/firebase';
 import { useRouter } from 'next/router';
 import { SuspensePage } from '@wallot/react/src/components/SuspensePage';
+import { getSsoWebAppRoute } from '@wallot/js';
 
 const Page: NextPage = () => {
 	const router = useRouter();
@@ -14,7 +15,14 @@ const Page: NextPage = () => {
 				await signOut(auth);
 				// Wait 1 second
 				await new Promise((resolve) => setTimeout(resolve, 1000));
-				await router.replace('/login');
+				await router.replace(
+					getSsoWebAppRoute({
+						includeOrigin: false,
+						origin: null,
+						queryParams: {},
+						routeStaticId: 'SSO_WEB_APP__/LOGIN',
+					}),
+				);
 			})(),
 		[],
 	);
