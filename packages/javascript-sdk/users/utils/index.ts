738ebfe3d07d9879534942d9d65378f4bb9ac79f
Kamar <kamarmack.home@gmail.com>
2024-12-19 07:34:38 +0000

    Add login user schema and export it from utils

--- packages/javascript-sdk/users/utils/index.ts
+++ packages/javascript-sdk/users/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './activateUserSchema.js';
+export * from './loginUserSchema.js';
 export * from './password.js';
 export * from './registerUserSchema.js';
 export * from './username.js';
