89d9bd31e009cb5f802b72a591617148ed9327c0
Kamar <kamarmack.home@gmail.com>
2024-12-19 05:45:54 +0000

    Add default values for age_range and capital_level in activateUserProperties schema

--- packages/javascript-sdk/users/utils/activateUserSchema.ts
+++ packages/javascript-sdk/users/utils/activateUserSchema.ts
@@ -1,10 +1,18 @@
 import * as yup from 'yup';
 import { Keys, getFieldSpecByFieldKey } from 'ergonomic';
 import { usersApi } from '../models/userProperties.js';
+import { AgeRange } from '../../utils/ageRange.js';
+import { CapitalLevel } from '../../utils/capitalLevel.js';
 
 export const activateUserProperties = {
-	age_range: usersApi.properties.age_range.defined().required(),
-	capital_level: usersApi.properties.capital_level.defined().required(),
+	age_range: usersApi.properties.age_range
+		.defined()
+		.required()
+		.default('' as AgeRange),
+	capital_level: usersApi.properties.capital_level
+		.defined()
+		.required()
+		.default('' as CapitalLevel),
 	investing_goals: usersApi.properties.investing_goals.defined().required(),
 	risk_preference: usersApi.properties.risk_preference
 		.defined()
