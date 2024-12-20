52ee3beba33340714c1670aa19314a8508275708
Kamar <kamarmack.home@gmail.com>
2024-12-20 23:28:50 +0000

    Remove PaymentMethods feature and related files from React and JavaScript SDKs

--- packages/react-sdk/src/features/paymentMethods/api/queryPaymentMethodPage.ts
+++
@@ -1,8 +0,0 @@
-import { GeneralizedApiResourceSpec } from 'ergonomic';
-import { generalizedFirestoreCollectionPageQuery } from 'ergonomic-react/src/features/data';
-import { PaymentMethod, paymentMethodsApi } from '@wallot/js';
-
-export const queryPaymentMethodPage =
-	generalizedFirestoreCollectionPageQuery<PaymentMethod>(
-		paymentMethodsApi as unknown as GeneralizedApiResourceSpec,
-	);
--- packages/react-sdk/src/features/paymentMethods/api/updatePaymentMethod.ts
+++
@@ -1,8 +0,0 @@
-import { generalizedFirestoreDocumentUpdateOperation } from 'ergonomic-react/src/features/data';
-import { UpdatePaymentMethodParams, paymentMethodsApi } from '@wallot/js';
-import { GeneralizedApiResourceSpec } from 'ergonomic';
-
-export const updatePaymentMethod =
-	generalizedFirestoreDocumentUpdateOperation<UpdatePaymentMethodParams>(
-		paymentMethodsApi as unknown as GeneralizedApiResourceSpec,
-	);
