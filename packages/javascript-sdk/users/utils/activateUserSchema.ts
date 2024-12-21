825b45067fe3a9c9b16cf2f9d34ab3bd6f33020d
Kamar <kamarmack.home@gmail.com>
2024-12-21 08:33:05 +0000

    Refactor activateUserSchema to enforce non-nullable properties and simplify ActivatedUser type definition

--- packages/javascript-sdk/users/utils/activateUserSchema.ts
+++ packages/javascript-sdk/users/utils/activateUserSchema.ts
@@ -1,5 +1,5 @@
 import * as yup from 'yup';
-import { Keys, WithNonNullableKeys, getFieldSpecByFieldKey } from 'ergonomic';
+import { Keys, getFieldSpecByFieldKey } from 'ergonomic';
 import { User, usersApi } from '../models/userProperties.js';
 import { AgeRange } from '../../utils/ageRange.js';
 import { CapitalLevel } from '../../utils/capitalLevel.js';
@@ -8,15 +8,21 @@ export const activateUserProperties = {
 	age_range: usersApi.properties.age_range
 		.defined()
 		.required()
+		.nullable(false)
 		.default('' as AgeRange),
 	capital_level: usersApi.properties.capital_level
 		.defined()
 		.required()
+		.nullable(false)
 		.default('' as CapitalLevel),
-	investing_goals: usersApi.properties.investing_goals.defined().required(),
+	investing_goals: usersApi.properties.investing_goals
+		.defined()
+		.required()
+		.nullable(false),
 	risk_preference: usersApi.properties.risk_preference
 		.defined()
 		.required()
+		.nullable(false)
 		.default('medium'),
 } as const;
 export const activateUserSchema = yup.object(activateUserProperties);
@@ -30,7 +36,7 @@ export type ActivateUserResponse = {
 	redirect_url: string;
 };
 
-export type ActivatedUser = WithNonNullableKeys<User, keyof ActivateUserParams>;
+export type ActivatedUser = User & ActivateUserParams;
 export const isActivatedUser = (user: User): user is ActivatedUser => {
 	try {
 		activateUserSchema.validateSync(user);
