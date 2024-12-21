e38831d76b83647a4ae277e4607bfd6a68a9fae2
Kamar <kamarmack.home@gmail.com>
2024-12-21 00:26:52 +0000

    Add utility function to extract asset properties from Alpaca asset

--- packages/javascript-sdk/assets/utils/index.ts
+++ packages/javascript-sdk/assets/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './alpacaAssets.js';
 export * from './alphaVantageCompanies.js';
+export * from './getAssetPropertiesFromAlpacaAsset.js';
 export * from './getAssetPropertiesFromAlphaVantageCompany.js';
