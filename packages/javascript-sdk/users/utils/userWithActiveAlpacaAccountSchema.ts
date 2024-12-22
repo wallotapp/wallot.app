da62ea157b797e260da3683854269b50f86133e7
Kamar <kamarmack.home@gmail.com>
2024-12-22 05:52:10 +0000

    Implement validation schema for active Alpaca accounts and enhance type definitions

--- packages/javascript-sdk/users/utils/userWithActiveAlpacaAccountSchema.ts
+++ packages/javascript-sdk/users/utils/userWithActiveAlpacaAccountSchema.ts
@@ -1,2 +1,31 @@
-export const userWithActiveAlpacaAccountSchema = true;
-export type UserWithActiveAlpacaAccount = Record<string, never>;
+import * as yup from 'yup';
+import { User } from '../models/userProperties.js';
+import { AlpacaAccountStatusEnum } from './alpacaAccounts.js';
+import { UserPendingAlpacaAccount } from './userPendingAlpacaAccountSchema.js';
+import { YupHelpers } from 'ergonomic';
+
+export const userWithActiveAlpacaAccountProperties = {
+	alpaca_account_status: YupHelpers.constant(
+		AlpacaAccountStatusEnum.obj.ACTIVE,
+	),
+};
+export const userWithActiveAlpacaAccountSchema = yup.object(
+	userWithActiveAlpacaAccountProperties,
+);
+export type UserWithActiveAlpacaAccountParams = yup.InferType<
+	typeof userWithActiveAlpacaAccountSchema
+>;
+
+export type UserWithActiveAlpacaAccount = UserPendingAlpacaAccount &
+	UserWithActiveAlpacaAccountParams;
+export const isUserWithActiveAlpacaAccount = (
+	user: User,
+): user is UserWithActiveAlpacaAccount => {
+	try {
+		userWithActiveAlpacaAccountSchema.validateSync(user);
+		return true;
+	} catch (error) {
+		console.error('Error detected in isUserWithActiveAlpacaAccount', error);
+		return false;
+	}
+};
