02c9cb286514137588bcc4b681cf8e1c5fe53e92
Kamar <kamarmack.home@gmail.com>
2024-12-22 06:44:19 +0000

    Implement schema and validation for pending Alpaca ACH transfers

--- packages/javascript-sdk/achTransfers/utils/achTransferPendingAlpacaQueueSchema.ts
+++ packages/javascript-sdk/achTransfers/utils/achTransferPendingAlpacaQueueSchema.ts
@@ -1,2 +1,31 @@
-export const achTransferPendingAlpacaQueueSchema = true;
-export type AchTransferPendingAlpacaQueue = Record<string, never>;
+import * as yup from 'yup';
+import { AchTransfer } from '../models/achTransferProperties.js';
+import { alpacaAchTransferProperties } from './alpacaAchTransfers.js';
+
+export const achTransferPendingAlpacaQueueProperties = {
+	alpaca_ach_transfer_id: alpacaAchTransferProperties.alpaca_ach_transfer_id
+		.min(1)
+		.nullable(false),
+	alpaca_ach_transfer_status:
+		alpacaAchTransferProperties.alpaca_ach_transfer_status.nullable(false),
+};
+export const achTransferPendingAlpacaQueueSchema = yup.object(
+	achTransferPendingAlpacaQueueProperties,
+);
+export type AchTransferPendingAlpacaQueueParams = yup.InferType<
+	typeof achTransferPendingAlpacaQueueSchema
+>;
+
+export type AchTransferPendingAlpacaQueue = AchTransfer &
+	AchTransferPendingAlpacaQueueParams;
+export const isAchTransferPendingAlpacaQueue = (
+	achTransfer: AchTransfer,
+): achTransfer is AchTransferPendingAlpacaQueue => {
+	try {
+		achTransferPendingAlpacaQueueSchema.validateSync(achTransfer);
+		return true;
+	} catch (error) {
+		console.error('Error detected in isAchTransferPendingAlpacaQueue', error);
+		return false;
+	}
+};
