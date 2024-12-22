4477e51a2b77e6e15fcbaae3524891a6b1a18464
Kamar <kamarmack.home@gmail.com>
2024-12-22 06:23:08 +0000

    Add utility function to retrieve user properties from Alpaca account

--- packages/javascript-sdk/users/utils/index.ts
+++ packages/javascript-sdk/users/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './activateUserSchema.js';
 export * from './alpacaAccounts.js';
 export * from './completeUserKycSchema.js';
+export * from './getUserPropertiesFromAlpacaAccount.js';
 export * from './loginUserSchema.js';
 export * from './password.js';
 export * from './registerUserSchema.js';
