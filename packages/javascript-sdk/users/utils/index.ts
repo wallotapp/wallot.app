36e8fc9df8723a0ca58c0063e1fbc84aeecf2513
Kamar <kamarmack.home@gmail.com>
2024-12-22 01:39:35 +0000

    Add userPendingAlpacaAccount schema and validation

--- packages/javascript-sdk/users/utils/index.ts
+++ packages/javascript-sdk/users/utils/index.ts
@@ -5,3 +5,4 @@ export * from './loginUserSchema.js';
 export * from './password.js';
 export * from './registerUserSchema.js';
 export * from './username.js';
+export * from './userPendingAlpacaAccount.js';
--- packages/javascript-sdk/users/utils/userPendingAlpacaAccount.ts
+++ packages/javascript-sdk/users/utils/userPendingAlpacaAccount.ts
@@ -1,5 +1,35 @@
+import * as yup from 'yup';
+import { User } from '../models/userProperties.js';
+import { alpacaAccountProperties } from './alpacaAccounts.js';
 import { KycUser } from './completeUserKycSchema';
 
-export type UserPendingAlpacaAccount = KycUser & {
-	alpaca_account_status: 'SUBMITTED';
+export const userPendingAlpacaAccountProperties = {
+	alpaca_account_account_number:
+		alpacaAccountProperties.alpaca_account_account_number
+			.min(1)
+			.nullable(false),
+	alpaca_account_id: alpacaAccountProperties.alpaca_account_id
+		.min(1)
+		.nullable(false),
+	alpaca_account_status: alpacaAccountProperties.alpaca_account_status
+		.min(1)
+		.nullable(false),
+};
+export const userPendingAlpacaAccountSchema = yup.object(
+	userPendingAlpacaAccountProperties,
+);
+export type UserPendingAlpacaAccountParams = yup.InferType<
+	typeof userPendingAlpacaAccountSchema
+>;
+
+export type UserPendingAlpacaAccount = KycUser & UserPendingAlpacaAccountParams;
+export const isUserPendingAlpacaAccount = (
+	user: User,
+): user is UserPendingAlpacaAccount => {
+	try {
+		userPendingAlpacaAccountSchema.validateSync(user);
+		return true;
+	} catch (error) {
+		return false;
+	}
 };
