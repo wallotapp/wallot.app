eea240e5cc9009d2d3ebdf0e7cd33cde052ec3f9
Kamar <kamarmack.home@gmail.com>
2024-12-22 06:21:26 +0000

    Add utility function to extract bank account properties from Alpaca ACH relationship

--- packages/javascript-sdk/bankAccounts/utils/index.ts
+++ packages/javascript-sdk/bankAccounts/utils/index.ts
@@ -2,3 +2,4 @@ export * from './alpacaAchRelationships.js';
 export * from './bankAccountApprovedByAlpacaSchema.js';
 export * from './bankAccountPendingAlpacaAchRelationshipSchema.js';
 export * from './bankAccountRejectedByAlpacaSchema.js';
+export * from './getBankAccountPropertiesFromAlpacaAchRelationship.js';
