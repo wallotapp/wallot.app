2135adf3bee61bbeeddb4ae9f15b37b432061110
Kamar <kamarmack.home@gmail.com>
2024-12-22 07:34:40 +0000

    fix field typo

--- packages/javascript-sdk/achTransfers/utils/achTransferRejectedByAlpacaSchema.ts
+++ packages/javascript-sdk/achTransfers/utils/achTransferRejectedByAlpacaSchema.ts
@@ -1,2 +1,35 @@
-export const achTransferRejectedByAlpacaSchema = true;
-export type AchTransferRejectedByAlpaca = Record<string, never>;
+import * as yup from 'yup';
+import { AchTransfer } from '../models/achTransferProperties.js';
+import { AlpacaAchTransferStatusEnum } from './alpacaAchTransfers.js';
+import { getEnum } from 'ergonomic';
+
+export const RejectedAlpacaAchTransferStatusEnum = getEnum([
+	AlpacaAchTransferStatusEnum.obj.CANCELED,
+	AlpacaAchTransferStatusEnum.obj.REJECTED,
+	AlpacaAchTransferStatusEnum.obj.RETURNED,
+]);
+
+export const achTransferRejectedByAlpacaProperties = {
+	alpaca_ach_transfer_status:
+		RejectedAlpacaAchTransferStatusEnum.getDefinedSchema(),
+};
+export const achTransferRejectedByAlpacaSchema = yup.object(
+	achTransferRejectedByAlpacaProperties,
+);
+export type AchTransferRejectedByAlpacaParams = yup.InferType<
+	typeof achTransferRejectedByAlpacaSchema
+>;
+
+export type AchTransferRejectedByAlpaca = AchTransfer &
+	AchTransferRejectedByAlpacaParams;
+export const isAchTransferRejectedByAlpaca = (
+	achTransfer: AchTransfer,
+): achTransfer is AchTransferRejectedByAlpaca => {
+	try {
+		achTransferRejectedByAlpacaSchema.validateSync(achTransfer);
+		return true;
+	} catch (error) {
+		console.error('Error detected in isAchTransferRejectedByAlpaca', error);
+		return false;
+	}
+};
