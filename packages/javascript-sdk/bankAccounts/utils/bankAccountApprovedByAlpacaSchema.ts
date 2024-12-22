6ce5c00a0b22eca0c97042e696c49e3c837c8d1f
Kamar <kamarmack.home@gmail.com>
2024-12-22 06:39:47 +0000

    Add schemas and validation for approved and rejected bank accounts by Alpaca

--- packages/javascript-sdk/bankAccounts/utils/bankAccountApprovedByAlpacaSchema.ts
+++ packages/javascript-sdk/bankAccounts/utils/bankAccountApprovedByAlpacaSchema.ts
@@ -1,2 +1,30 @@
-export const bankAccountApprovedByAlpacaSchema = true;
-export type BankAccountApprovedByAlpaca = Record<string, never>;
+import * as yup from 'yup';
+import { BankAccount } from '../models/bankAccountProperties.js';
+import { AlpacaAchRelationshipStatusEnum } from './alpacaAchRelationships.js';
+import { YupHelpers } from 'ergonomic';
+
+export const bankAccountApprovedByAlpacaProperties = {
+	alpaca_ach_relationship_status: YupHelpers.constant(
+		AlpacaAchRelationshipStatusEnum.obj.APPROVED,
+	),
+};
+export const bankAccountApprovedByAlpacaSchema = yup.object(
+	bankAccountApprovedByAlpacaProperties,
+);
+export type BankAccountApprovedByAlpacaParams = yup.InferType<
+	typeof bankAccountApprovedByAlpacaSchema
+>;
+
+export type BankAccountApprovedByAlpaca = BankAccount &
+	BankAccountApprovedByAlpacaParams;
+export const isBankAccountApprovedByAlpaca = (
+	bankAccount: BankAccount,
+): bankAccount is BankAccountApprovedByAlpaca => {
+	try {
+		bankAccountApprovedByAlpacaSchema.validateSync(bankAccount);
+		return true;
+	} catch (error) {
+		console.error('Error detected in isBankAccountApprovedByAlpaca', error);
+		return false;
+	}
+};
--- packages/javascript-sdk/bankAccounts/utils/bankAccountRejectedByAlpacaSchema.ts
+++ packages/javascript-sdk/bankAccounts/utils/bankAccountRejectedByAlpacaSchema.ts
@@ -1,2 +1,30 @@
-export const bankAccountRejectedByAlpacaSchema = true;
-export type BankAccountRejectedByAlpaca = Record<string, never>;
+import * as yup from 'yup';
+import { BankAccount } from '../models/bankAccountProperties.js';
+import { AlpacaAchRelationshipStatusEnum } from './alpacaAchRelationships.js';
+import { YupHelpers } from 'ergonomic';
+
+export const bankAccountRejectedByAlpacaProperties = {
+	alpaca_ach_relationship_status: YupHelpers.constant(
+		AlpacaAchRelationshipStatusEnum.obj.CANCELED,
+	),
+};
+export const bankAccountRejectedByAlpacaSchema = yup.object(
+	bankAccountRejectedByAlpacaProperties,
+);
+export type BankAccountRejectedByAlpacaParams = yup.InferType<
+	typeof bankAccountRejectedByAlpacaSchema
+>;
+
+export type BankAccountRejectedByAlpaca = BankAccount &
+	BankAccountRejectedByAlpacaParams;
+export const isBankAccountRejectedByAlpaca = (
+	bankAccount: BankAccount,
+): bankAccount is BankAccountRejectedByAlpaca => {
+	try {
+		bankAccountRejectedByAlpacaSchema.validateSync(bankAccount);
+		return true;
+	} catch (error) {
+		console.error('Error detected in isBankAccountRejectedByAlpaca', error);
+		return false;
+	}
+};
