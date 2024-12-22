cb1ea9eafc281866a070d1eed6f136903a09940b
Kamar <kamarmack.home@gmail.com>
2024-12-22 08:39:46 +0000

    Add validation schemas for filled and rejected Alpaca orders

--- packages/javascript-sdk/assetOrders/utils/assetOrderFilledByAlpacaSchema.ts
+++ packages/javascript-sdk/assetOrders/utils/assetOrderFilledByAlpacaSchema.ts
@@ -1,2 +1,29 @@
-export const assetOrderFilledByAlpacaSchema = true;
-export type AssetOrderFilledByAlpaca = Record<string, never>;
+import * as yup from 'yup';
+import { AssetOrder } from '../models/assetOrderProperties.js';
+import { AlpacaOrderStatusEnum } from './alpacaOrders.js';
+import { YupHelpers } from 'ergonomic';
+import { AssetOrderPendingAlpacaFill } from './assetOrderPendingAlpacaFillSchema.js';
+
+export const assetOrderFilledByAlpacaProperties = {
+	alpaca_order_status: YupHelpers.constant(AlpacaOrderStatusEnum.obj.filled),
+};
+export const assetOrderFilledByAlpacaSchema = yup.object(
+	assetOrderFilledByAlpacaProperties,
+);
+export type AssetOrderFilledByAlpacaParams = yup.InferType<
+	typeof assetOrderFilledByAlpacaSchema
+>;
+
+export type AssetOrderFilledByAlpaca = AssetOrderPendingAlpacaFill &
+	AssetOrderFilledByAlpacaParams;
+export const isAssetOrderFilledByAlpaca = (
+	assetOrder: AssetOrder,
+): assetOrder is AssetOrderFilledByAlpaca => {
+	try {
+		assetOrderFilledByAlpacaSchema.validateSync(assetOrder);
+		return true;
+	} catch (error) {
+		console.error('Error detected in isAssetOrderFilledByAlpaca', error);
+		return false;
+	}
+};
--- packages/javascript-sdk/assetOrders/utils/assetOrderRejectedByAlpacaSchema.ts
+++ packages/javascript-sdk/assetOrders/utils/assetOrderRejectedByAlpacaSchema.ts
@@ -1,2 +1,39 @@
-export const assetOrderRejectedByAlpacaSchema = true;
-export type AssetOrderRejectedByAlpaca = Record<string, never>;
+import * as yup from 'yup';
+import { AssetOrder } from '../models/assetOrderProperties.js';
+import { AlpacaOrderStatusEnum } from './alpacaOrders.js';
+import { getEnum } from 'ergonomic';
+import { AssetOrderPendingAlpacaFill } from './assetOrderPendingAlpacaFillSchema.js';
+
+export const RejectedAlpacaOrderStatusEnum = getEnum([
+	AlpacaOrderStatusEnum.obj.canceled,
+	AlpacaOrderStatusEnum.obj.expired,
+	AlpacaOrderStatusEnum.obj.pending_cancel,
+	AlpacaOrderStatusEnum.obj.rejected,
+	AlpacaOrderStatusEnum.obj.replaced,
+	AlpacaOrderStatusEnum.obj.stopped,
+	AlpacaOrderStatusEnum.obj.suspended,
+]);
+
+export const assetOrderRejectedByAlpacaProperties = {
+	alpaca_order_status: RejectedAlpacaOrderStatusEnum.getDefinedSchema(),
+};
+export const assetOrderRejectedByAlpacaSchema = yup.object(
+	assetOrderRejectedByAlpacaProperties,
+);
+export type AssetOrderRejectedByAlpacaParams = yup.InferType<
+	typeof assetOrderRejectedByAlpacaSchema
+>;
+
+export type AssetOrderRejectedByAlpaca = AssetOrderPendingAlpacaFill &
+	AssetOrderRejectedByAlpacaParams;
+export const isAssetOrderRejectedByAlpaca = (
+	assetOrder: AssetOrder,
+): assetOrder is AssetOrderRejectedByAlpaca => {
+	try {
+		assetOrderRejectedByAlpacaSchema.validateSync(assetOrder);
+		return true;
+	} catch (error) {
+		console.error('Error detected in isAssetOrderRejectedByAlpaca', error);
+		return false;
+	}
+};
