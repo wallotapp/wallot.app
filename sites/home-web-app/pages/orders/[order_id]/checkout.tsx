1dd54dffcbaa843bb110f83272187561c4603b05
Kamar <kamarmack.home@gmail.com>
2024-12-23 07:35:10 +0000

    Implement checkout page enhancements with order summary and total calculation

--- sites/home-web-app/pages/orders/[order_id]/checkout.tsx
+++ sites/home-web-app/pages/orders/[order_id]/checkout.tsx
@@ -5,7 +5,15 @@ import {
 	PageStaticProps,
 	PageProps,
 } from 'ergonomic-react/src/components/nextjs-pages/Page';
-import { HomeWebAppRouteQueryParams } from '@wallot/js';
+import { default as cn } from 'ergonomic-react/src/lib/cn';
+import { getHomeWebAppRoute, HomeWebAppRouteQueryParams } from '@wallot/js';
+import { useQueryAssetOrderPage } from '@wallot/react/src/features/assetOrders';
+import { AuthenticatedPageHeader } from '@wallot/react/src/components/AuthenticatedPageHeader';
+import { PageActionHeader } from '@wallot/react/src/components/PageActionHeader';
+import { getCurrencyUsdStringFromCents } from 'ergonomic';
+import Link from 'next/link';
+import { useSiteOriginByTarget } from '@wallot/react/src/hooks/useSiteOriginByTarget';
+import { Separator } from 'ergonomic-react/src/components/ui/separator';
 
 // ==== Static Page Props ==== //
 
@@ -27,6 +35,9 @@ const Page: NextPage = () => {
 	// Router
 	const router = useRouter();
 
+	// Site Origin by Target
+	const siteOriginByTarget = useSiteOriginByTarget();
+
 	// ==== Constants ==== //
 
 	// Router Query
@@ -47,11 +58,110 @@ const Page: NextPage = () => {
 		routeId: ROUTE_RUNTIME_ID,
 	};
 
+	// ==== Hooks ==== //
+	const { data: assetOrderPage } = useQueryAssetOrderPage({
+		firestoreQueryOptions: {
+			whereClauses: [['order', '==', order_id]],
+		},
+	});
+	const assetOrders = assetOrderPage?.documents ?? [];
+	const assetTotalAmount = assetOrders.reduce((acc, assetOrder) => {
+		return acc + Number(assetOrder.amount);
+	}, 0);
+	const assetTotalAmountUsdString = getCurrencyUsdStringFromCents(
+		assetTotalAmount + 2499,
+	);
+
 	// ==== Render ==== //
 	return (
 		<PageComponent {...pageProps}>
-			<div>
-				<div>Checkout</div>
+			<div className={cn('flex flex-col min-h-screen min-w-screen relative')}>
+				<AuthenticatedPageHeader showHomeLink={false} />
+				<PageActionHeader />
+				<div
+					className={cn(
+						'min-h-[95vh] w-full',
+						'py-48 px-6',
+						'lg:py-48 lg:px-28',
+					)}
+				>
+					<div>
+						<div className={cn('lg:flex lg:justify-between lg:space-x-28')}>
+							<div className='lg:w-3/5'>
+								<div>
+									<p className='font-semibold text-3xl'>Checkout</p>
+								</div>
+							</div>
+							<div
+								className={cn(
+									'bg-slate-100 mt-8 px-10 py-10 rounded-xl h-fit',
+									'lg:w-2/5',
+								)}
+							>
+								<div>
+									<p className='font-semibold text-xl'>Order Summary</p>
+								</div>
+								<div className='mt-2.5'>
+									<p className='font-medium text-base'>
+										{assetOrders.length} item{assetOrders.length > 1 ? 's' : ''}
+									</p>
+								</div>
+								<div className='mt-4'>
+									<Separator />
+								</div>
+								<div className='mt-6 flex justify-between'>
+									<div>
+										<p className='font-semibold text-xl'>
+											Total{' '}
+											<span className='text-sm text-brand-dark'>(USD)</span>
+										</p>
+									</div>
+									<div>
+										<p className='font-medium text-xl'>
+											{assetTotalAmountUsdString}
+										</p>
+									</div>
+								</div>
+								<div className='mt-6'>
+									<Link
+										href={getHomeWebAppRoute({
+											includeOrigin: true,
+											origin: siteOriginByTarget.HOME_WEB_APP,
+											queryParams: { asset_id: 'todo' },
+											routeStaticId:
+												'HOME_WEB_APP__/ASSETS/[ASSET_ID]/CONGRATULATIONS',
+										})}
+									>
+										<button className='w-full'>
+											<div className='bg-black py-2.5 px-10 rounded-sm flex items-center justify-center space-x-2'>
+												<p className='text-sm text-white dark:text-brand'>
+													Complete Purchase
+												</p>
+											</div>
+										</button>
+									</Link>
+								</div>
+								<div className='mt-6'>
+									<p className='font-normal text-sm'>
+										By clicking "Complete Purchase", you agree to our{' '}
+										<Link href='/terms'>
+											<span className='underline'>Terms of Service</span>
+										</Link>{' '}
+										and{' '}
+										<Link href='/privacy'>
+											<span className='underline'>Privacy Policy</span>
+										</Link>
+										, and affirm that the tax information you provided is
+										accurate and complete to the best of your knowledge. In
+										addition, you agree that you are conducting trading activity
+										at your own risk and that you are responsible for any losses
+										that may occur.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</PageComponent>
 	);
