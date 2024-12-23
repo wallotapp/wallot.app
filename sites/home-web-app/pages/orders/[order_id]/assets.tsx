c118baa5fd20d58737a26b5a144cf9b463cd131a
Kamar <kamarmack.home@gmail.com>
2024-12-23 01:45:55 +0000

    Add AssetOrderCard component and integrate asset order retrieval in orders page

--- sites/home-web-app/pages/orders/[order_id]/assets.tsx
+++ sites/home-web-app/pages/orders/[order_id]/assets.tsx
@@ -6,6 +6,14 @@ import {
 	PageProps,
 } from 'ergonomic-react/src/components/nextjs-pages/Page';
 import { HomeWebAppRouteQueryParams } from '@wallot/js';
+import { AssetOrder } from '@wallot/js';
+import { useQueryAssetOrderPage } from '@wallot/react/src/features/assetOrders';
+
+const AssetOrderCard: React.FC<{ assetOrder: AssetOrder }> = ({
+	assetOrder,
+}) => {
+	return <div>{assetOrder._id}</div>;
+};
 
 // ==== Static Page Props ==== //
 
@@ -47,11 +55,24 @@ const Page: NextPage = () => {
 		routeId: ROUTE_RUNTIME_ID,
 	};
 
+	// ==== Hooks ==== //
+	const { data: assetOrderPage } = useQueryAssetOrderPage({
+		firestoreQueryOptions: {
+			whereClauses: [['order', '==', order_id]],
+		},
+	});
+	const assetOrders = assetOrderPage?.documents ?? [];
+
 	// ==== Render ==== //
 	return (
 		<PageComponent {...pageProps}>
 			<div>
 				<div>Assets</div>
+				{assetOrders.map((assetOrder) => {
+					return (
+						<AssetOrderCard key={assetOrder._id} assetOrder={assetOrder} />
+					);
+				})}
 			</div>
 		</PageComponent>
 	);
