4468faf88166227d8af06fe50142b1161f8f60d4
Kamar <kamarmack.home@gmail.com>
2024-12-23 02:52:54 +0000

    Add custom hook for querying current authentication credentials

--- packages/react-sdk/src/features/authCredentials/index.tsx
+++ packages/react-sdk/src/features/authCredentials/index.tsx
@@ -1,4 +1,5 @@
 export * from '@wallot/react/src/features/authCredentials/hooks/useCreateAuthCredentialMutation';
 export * from '@wallot/react/src/features/authCredentials/hooks/useQueryAuthCredentialPage';
+export * from '@wallot/react/src/features/authCredentials/hooks/useQueryCurrentAuthCredential';
 export * from '@wallot/react/src/features/authCredentials/hooks/useUpdateAuthCredentialMutation';
 export * from '@wallot/react/src/features/authCredentials/types/AuthCredentialReactTypes';
