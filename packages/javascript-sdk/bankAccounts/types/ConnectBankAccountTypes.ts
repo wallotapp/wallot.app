9d86bbf97c65f6a63c770419df04f83213812ea8
Kamar <kamarmack.home@gmail.com>
2024-12-24 05:52:18 +0000

    Format project

--- packages/javascript-sdk/bankAccounts/types/ConnectBankAccountTypes.ts
+++ packages/javascript-sdk/bankAccounts/types/ConnectBankAccountTypes.ts
@@ -1,4 +1,4 @@
 export type ConnectBankAccountsParams = {
-  stripe_financial_connections_account_ids: string[];
+	stripe_financial_connections_account_ids: string[];
 };
 export type ConnectBankAccountsResponse = Record<string, never>;
