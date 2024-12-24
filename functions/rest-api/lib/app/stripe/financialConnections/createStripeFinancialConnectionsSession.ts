b0432589c7a4362d18a37091479173ad6cc866b8
Kamar <kamarmack.home@gmail.com>
2024-12-24 04:36:34 +0000

    Add createStripeFinancialConnectionsSession function for Stripe integration

---
+++ functions/rest-api/lib/app/stripe/financialConnections/createStripeFinancialConnectionsSession.ts
@@ -0,0 +1,41 @@
+import { type Stripe } from 'stripe';
+import { type DecodedIdToken as FirebaseUser } from 'firebase-admin/auth';
+import { FunctionResponse } from '@wallot/node';
+import { User, usersApi } from '@wallot/js';
+import { db, stripe } from '../../../services.js';
+
+export const createStripeFinancialConnectionsSession = async (
+	_body: Record<string, never>,
+	_params: Record<string, never>,
+	_query: Record<string, never>,
+	firebaseUser: FirebaseUser | null,
+): Promise<FunctionResponse<{ client_secret: string }>> => {
+	if (!firebaseUser) throw new Error('Unauthorized');
+
+	// Locate USER by Firebase UID
+	const usersCollectionId = usersApi.collectionId;
+	const user = (
+		await db.collection(usersCollectionId).doc(firebaseUser.uid).get()
+	).data() as User;
+	const { stripe_customer_id } = user;
+
+	// Create a Financial Connections session
+	const session = await stripe.financialConnections.sessions.create({
+		account_holder: {
+			customer: stripe_customer_id,
+			type: 'customer',
+		},
+		permissions: [
+			'account_numbers' as unknown as Stripe.FinancialConnections.SessionCreateParams.Permission,
+			'balances',
+			'ownership',
+			'payment_method',
+			'transactions',
+		],
+	});
+	if (session.client_secret == null)
+		throw new Error('Session does not have a client secret');
+
+	// Return the session client secret
+	return { json: { client_secret: session.client_secret } };
+};
