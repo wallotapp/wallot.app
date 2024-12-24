5d0836c58119ed7adbaaa278865323ac255d6aa8
Kamar <kamarmack.home@gmail.com>
2024-12-24 05:33:01 +0000

    Add connectBankAccounts function to handle bank account connections

---
+++ packages/react-sdk/src/features/bankAccounts/api/connectBankAccounts.ts
@@ -0,0 +1,28 @@
+import { User as FirebaseUser } from 'firebase/auth';
+import { handleKyError } from 'ergonomic';
+import { getAuthenticatedKyInstance } from '@wallot/react/src/lib/ky';
+import {
+	ConnectBankAccountsParams,
+	ConnectBankAccountsResponse,
+} from '@wallot/js';
+
+export const connectBankAccounts = async (
+	firebaseUser: FirebaseUser | null,
+	params: ConnectBankAccountsParams,
+) => {
+	try {
+		if (!firebaseUser) {
+			throw new Error('User is not authenticated');
+		}
+
+		const data = await getAuthenticatedKyInstance(firebaseUser)
+			.post('v0/bank-accounts/connect', {
+				json: params,
+			})
+			.json<ConnectBankAccountsResponse>();
+		return data;
+	} catch (err) {
+		const kyErr = await handleKyError(err);
+		return Promise.reject(kyErr);
+	}
+};
