320acb112da41915768c23a5be00f4216b1c2cd7
Kamar <kamarmack.home@gmail.com>
2024-12-25 03:01:23 +0000

    Fix encryption key handling in encryptString and decryptString functions

--- packages/node-sdk/crypto/decryptString.ts
+++ packages/node-sdk/crypto/decryptString.ts
@@ -9,7 +9,7 @@ export const decryptString =
 	(encryptionKey: string) =>
 	({ data, ivHex }: EncryptedData) => {
 		const iv = Buffer.from(ivHex, 'hex');
-		const decipher = crypto.createDecipheriv(encryptionAlgorithm, encryptionKey, iv);
+		const decipher = crypto.createDecipheriv(encryptionAlgorithm, Buffer.from(encryptionKey, 'hex'), iv);
 		let decrypted = decipher.update(data, 'hex', 'utf8');
 		decrypted += decipher.final('utf8');
 		return decrypted;
