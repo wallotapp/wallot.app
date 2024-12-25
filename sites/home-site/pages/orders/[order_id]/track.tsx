d9821fe1a819a3cb46acd55126409c6414e9e8d3
Kamar <kamarmack.home@gmail.com>
2024-12-25 13:32:15 +0000

    feat: enhance order tracking page with asset order details and loading skeleton

--- sites/home-site/pages/orders/[order_id]/track.tsx
+++ sites/home-site/pages/orders/[order_id]/track.tsx
@@ -5,9 +5,21 @@ import {
 	PageStaticProps,
 	PageProps,
 } from 'ergonomic-react/src/components/nextjs-pages/Page';
-import { HomeSiteRouteQueryParams, getSsoSiteRoute } from '@wallot/js';
-import { useAuthenticatedRouteRedirect } from 'ergonomic-react/src/features/authentication/hooks/useAuthenticatedRouteRedirect';
+import { Skeleton } from 'ergonomic-react/src/components/ui/skeleton';
+import { default as cn } from 'ergonomic-react/src/lib/cn';
+import {
+	HomeSiteRouteQueryParams,
+	getSsoSiteRoute
+} from '@wallot/js';
+import { AssetOrderCartItem } from '@wallot/react/src/features/assetOrders';
+import { useQueryAssetOrderPage } from '@wallot/react/src/features/assetOrders';
+import { AuthenticatedPageHeader } from '@wallot/react/src/components/AuthenticatedPageHeader';
+import { PageActionHeader } from '@wallot/react/src/components/PageActionHeader';
+import { Fragment } from 'react';
+import { getCurrencyUsdStringFromCents } from 'ergonomic';
 import { useSiteOriginByTarget } from '@wallot/react/src/hooks/useSiteOriginByTarget';
+import { Separator } from 'ergonomic-react/src/components/ui/separator';
+import { useAuthenticatedRouteRedirect } from 'ergonomic-react/src/features/authentication/hooks/useAuthenticatedRouteRedirect';
 
 // ==== Static Page Props ==== //
 
@@ -63,11 +75,104 @@ const Page: NextPage = () => {
 		routeId: ROUTE_RUNTIME_ID,
 	};
 
+	// ==== Hooks ==== //
+	const { data: assetOrderPage, isLoading: isAssetOrderPageLoading } =
+		useQueryAssetOrderPage({
+			firestoreQueryOptions: {
+				whereClauses: [['order', '==', order_id]],
+			},
+		});
+	const assetOrders = assetOrderPage?.documents ?? [];
+	const assetTotalAmount = assetOrders.reduce((acc, assetOrder) => {
+		return acc + assetOrder.amount;
+	}, 0);
+	const assetTotalAmountUsdString =
+		getCurrencyUsdStringFromCents(assetTotalAmount);
+	const isDataLoading = isAssetOrderPageLoading;
+
 	// ==== Render ==== //
 	return (
 		<PageComponent {...pageProps}>
-			<div>
-				<div>Track</div>
+			<div className={cn('flex flex-col min-h-screen min-w-screen relative')}>
+				<AuthenticatedPageHeader showHomeLink={false} />
+				<PageActionHeader />
+				<div
+					className={cn(
+						'min-h-[95vh] w-full',
+						'py-48 px-6',
+						'lg:py-48 lg:px-28',
+					)}
+				>
+					<div>
+						<div className={cn('lg:flex lg:justify-between lg:space-x-28')}>
+							<div className='lg:w-3/5'>
+								<div>
+									<p className='font-semibold text-3xl'>Your Order</p>
+									<p className='font-medium text-base'>
+										Below is a list of the stocks included in this order.
+									</p>
+								</div>
+								<div className='mt-6'>
+									{isDataLoading ? (
+										<Fragment>
+											{[1, 2, 3, 4].map((_, index) => (
+												<div className=''>
+													<Skeleton
+														className='h-[30rem] !bg-gray-300'
+														key={index}
+													/>
+												</div>
+											))}
+										</Fragment>
+									) : (
+										<Fragment>
+											{assetOrders.map((assetOrder, assetOrderIdx) => {
+												return (
+													<AssetOrderCartItem
+														assetOrder={assetOrder}
+														className={cn(assetOrderIdx === 0 ? '' : 'mt-4')}
+														key={assetOrder._id}
+													/>
+												);
+											})}
+										</Fragment>
+									)}
+								</div>
+							</div>
+							<div
+								className={cn(
+									'bg-slate-100 mt-8 px-10 py-10 rounded-xl h-fit',
+									'lg:w-2/5',
+								)}
+							>
+								<div>
+									<p className='font-semibold text-xl'>Order Summary</p>
+								</div>
+								<div className='mt-2.5'>
+									<p className='font-medium text-base'>
+										{assetOrders.length} item{assetOrders.length > 1 ? 's' : ''}
+									</p>
+								</div>
+								<div className='mt-4'>
+									<Separator />
+								</div>
+								<div className='mt-6 flex justify-between'>
+									<div>
+										<p className='font-semibold text-xl'>
+											Subtotal{' '}
+											<span className='text-sm text-brand-dark'>(USD)</span>
+										</p>
+									</div>
+									<div>
+										<p className='font-medium text-xl'>
+											{assetTotalAmountUsdString}
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</PageComponent>
 	);
