df8524c95e90289ebec4f40d6f219b1f18faa816
Kamar <kamarmack.home@gmail.com>
2024-12-25 06:37:35 +0000

    Enhance congratulations page to display user-specific order details and stock purchase status

--- sites/home-web-app/pages/assets/[asset_id]/congratulations.tsx
+++ sites/home-web-app/pages/assets/[asset_id]/congratulations.tsx
@@ -4,6 +4,12 @@ import { Page as PageComponent, PageStaticProps, PageProps } from 'ergonomic-rea
 import { HomeWebAppRouteQueryParams, getSsoWebAppRoute } from '@wallot/js';
 import { useAuthenticatedRouteRedirect } from 'ergonomic-react/src/features/authentication/hooks/useAuthenticatedRouteRedirect';
 import { useSiteOriginByTarget } from '@wallot/react/src/hooks/useSiteOriginByTarget';
+import { default as cn } from 'ergonomic-react/src/lib/cn';
+import { PlatformLogo } from 'ergonomic-react/src/components/brand/PlatformLogo';
+import { OPEN_GRAPH_CONFIG } from 'ergonomic-react/src/config/openGraphConfig';
+import Link from 'next/link';
+import { GoCheck } from 'react-icons/go';
+import { useQueryCurrentUser } from '@wallot/react/src/features/users';
 
 // ==== Static Page Props ==== //
 
@@ -39,6 +45,9 @@ const Page: NextPage = () => {
 	// Router
 	const router = useRouter();
 
+	// Current User
+	const { currentUser } = useQueryCurrentUser();
+
 	// ==== Constants ==== //
 
 	// Router Query
@@ -59,8 +68,46 @@ const Page: NextPage = () => {
 	// ==== Render ==== //
 	return (
 		<PageComponent {...pageProps}>
-			<div>
-				<div>Congratulations</div>
+			<div className={cn('min-h-screen relative', 'px-8 pt-24')}>
+				<div className='mb-10 flex items-center justify-center'>
+					{OPEN_GRAPH_CONFIG.siteBrandLogoDarkMode && OPEN_GRAPH_CONFIG.siteBrandLogoLightMode && (
+						<PlatformLogo
+							height={380}
+							size='xl'
+							srcMap={{
+								dark: OPEN_GRAPH_CONFIG.siteBrandLogoDarkMode,
+								light: OPEN_GRAPH_CONFIG.siteBrandLogoLightMode,
+							}}
+							width={2048}
+						/>
+					)}
+					{!(OPEN_GRAPH_CONFIG.siteBrandLogoDarkMode && OPEN_GRAPH_CONFIG.siteBrandLogoLightMode) && (
+						<div>
+							<p className={cn('text-2xl font-bold', 'lg:text-3xl')}>{OPEN_GRAPH_CONFIG.siteName}</p>
+						</div>
+					)}
+				</div>
+				<div>
+					<p>AAPL is yours!</p>
+				</div>
+				<div>
+					<p>Stock purchase in progress. Sit back and let your money work for you.</p>
+				</div>
+				<div>
+					<Link href={`/assets/${asset_id}/track`}>
+						<p>Continue</p>
+					</Link>
+				</div>
+				<div>
+					<div>
+						<GoCheck />
+					</div>
+					<div>Thank you for your order!</div>
+					<div>We've emailed your receipt to {currentUser?.firebase_auth_email ?? 'your email on file'}</div>
+					<div>AAPL shares: $100.00</div>
+					<div>Taxes and fees: $24.99</div>
+					<div>Total: $124.99</div>
+				</div>
 			</div>
 		</PageComponent>
 	);
