9d3b9ab8c01574d29732ba81a810ac7a2f08f2ee
Kamar <kamarmack.home@gmail.com>
2024-12-26 22:34:41 +0000

    feat: implement createAlpacaAccount function for account creation

--- functions/cloud-tasks/lib/app/alpacaAccounts/createAlpacaAccount.ts
+++ functions/cloud-tasks/lib/app/alpacaAccounts/createAlpacaAccount.ts
@@ -1,5 +1,8 @@
 import { CloudTaskHandler } from 'ergonomic-node';
 import { CreateAlpacaAccountTaskParams } from '@wallot/node';
+import { AlpacaAccount, KycUser } from '@wallot/js';
+import { alpaca } from '../../services.js';
+import { getUtcDateNow } from 'ergonomic';
 
 export const handleCreateAlpacaAccountTaskOptions = {
 	rateLimits: { maxConcurrentDispatches: 6 },
@@ -15,3 +18,25 @@ export const handleCreateAlpacaAccountTask: CloudTaskHandler<
 	userId;
 	return Promise.resolve();
 };
+
+async function createAlpacaAccount(user: KycUser) {
+	const createAlpacaAccountParams: Pick<
+		AlpacaAccount,
+		'agreements' | 'contact' | 'disclosures' | 'identity'
+	> = {
+		agreements: [
+			{
+				agreement: 'customer_agreement',
+				ip_address: '127.0.0.1',
+				signed_at: getUtcDateNow(),
+			},
+		],
+		contact: user.alpaca_account_contact,
+		disclosures: user.alpaca_account_disclosures,
+		identity: user.alpaca_account_identity,
+	};
+	const response = await alpaca.broker.post<AlpacaAccount>('v1/accounts', {
+		json: createAlpacaAccountParams,
+	});
+	return response.json();
+}
