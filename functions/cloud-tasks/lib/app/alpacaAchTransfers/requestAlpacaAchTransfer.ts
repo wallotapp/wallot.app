ef1d7f2c406f0dfb72193120458282a3910543b5
Kamar <kamarmack.home@gmail.com>
2024-12-26 15:13:57 +0000

    feat: write createAlpacaAchRelationship function

--- functions/cloud-tasks/lib/app/alpacaAchTransfers/requestAlpacaAchTransfer.ts
+++ functions/cloud-tasks/lib/app/alpacaAchTransfers/requestAlpacaAchTransfer.ts
@@ -6,9 +6,12 @@ import {
 	BankAccount,
 	isOrderConfirmedByUser,
 	isBankAccountApprovedByAlpaca,
+	AlpacaAchRelationship,
+	UserWithAlpacaEquity,
+	TokenizedBankAccount,
 } from '@wallot/js';
 import { PlaceAlpacaOrdersTaskParams } from '@wallot/node';
-import { db, gcp } from '../../services.js';
+import { alpaca, crypto, db, gcp } from '../../services.js';
 
 export const handleRequestAlpacaAchTransferTaskOptions = {
 	rateLimits: { maxConcurrentDispatches: 6 },
@@ -58,3 +61,26 @@ export const handleRequestAlpacaAchTransfer: CloudTaskHandler<
 	// Task complete
 	return Promise.resolve();
 };
+
+async function createAlpacaAchRelationship(
+	user: UserWithAlpacaEquity,
+	bankAccount: TokenizedBankAccount,
+) {
+	const response = await alpaca.broker.post<AlpacaAchRelationship>(
+		`v1/accounts/${user.alpaca_account_id}/ach_relationships`,
+		{
+			json: {
+				account_owner_name: bankAccount.account_owner_name,
+				bank_account_type: bankAccount.account_type,
+				bank_account_number: crypto.decrypt({
+					data: bankAccount.account_number_data,
+					ivHex: bankAccount.account_number_iv_hex,
+				}),
+				bank_routing_number: bankAccount.routing_number,
+				// nickname: '',
+			},
+		},
+	);
+	return response.json();
+}
+createAlpacaAchRelationship;
