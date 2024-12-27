34b35a417b55f06da6a668538c3b0c05d4547beb
Kamar <kamarmack.home@gmail.com>
2024-12-27 17:42:45 +0000

    feat: add retrieveAssetPrice function

---
+++ packages/react-sdk/src/features/assetOrders/api/retrieveAssetPrice.ts
@@ -0,0 +1,28 @@
+import { User as FirebaseUser } from 'firebase/auth';
+import { handleKyError } from 'ergonomic';
+import { getAuthenticatedKyInstance } from '@wallot/react/src/lib/ky';
+import {
+	RetrieveAssetPriceResponse,
+	RetrieveAssetPriceQueryParams,
+} from '@wallot/js';
+
+export const retrieveAssetPrice = async (
+	firebaseUser: FirebaseUser | null,
+	searchParams: RetrieveAssetPriceQueryParams,
+) => {
+	try {
+		if (!firebaseUser) {
+			throw new Error('User is not authenticated');
+		}
+
+		const data = await getAuthenticatedKyInstance(firebaseUser)
+			.post('v0/assets/price', {
+				searchParams,
+			})
+			.json<RetrieveAssetPriceResponse>();
+		return data;
+	} catch (err) {
+		const kyErr = await handleKyError(err);
+		return Promise.reject(kyErr);
+	}
+};
