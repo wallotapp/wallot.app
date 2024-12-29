4fdccdada8a411ec28fe015246705fb055ddf663
Kamar <kamarmack.home@gmail.com>
2024-12-29 13:41:55 +0000

    feat: enhance useQueryLoggedInUserStatus with additional user state checks and task management

--- packages/react-sdk/src/features/users/hooks/useQueryLoggedInUserStatus.tsx
+++ packages/react-sdk/src/features/users/hooks/useQueryLoggedInUserStatus.tsx
@@ -1,5 +1,10 @@
 import {
 	BankAccount,
+	getHomeSiteRoute,
+	isActivatedUser,
+	isBankAccountTokenized,
+	isKycUser,
+	isOrderConfirmedByUser,
 	License,
 	Order,
 	Position,
@@ -17,14 +22,12 @@ import { useQueryRecommendationsForLoggedInUser } from '@wallot/react/src/featur
 export type LoggedInUserStatus = {
 	isLoggedInUserStatusLoading: boolean;
 	state: UserExperienceState;
-	tasks:
-		| {
-				ctaHref: string;
-				ctaText: string;
-				subtitle: string;
-				title: string;
-		  }[]
-		| null;
+	tasks: {
+		ctaHref: string;
+		ctaText: string;
+		subtitle: string;
+		title: string;
+	}[];
 };
 
 export const useQueryLoggedInUserStatus = (): LoggedInUserStatus => {
@@ -71,13 +74,13 @@ function getLoggedInUserStatus({
 	isLoggedInUserLoading,
 	bankAccountsForLoggedInUser,
 	isBankAccountPageLoading,
-	licensesForLoggedInUser,
+	// licensesForLoggedInUser,
 	isLicensePageLoading,
 	ordersForLoggedInUser,
 	isOrderPageLoading,
-	positionsForLoggedInUser,
+	// positionsForLoggedInUser,
 	isPositionPageLoading,
-	recommendationsForLoggedInUser,
+	// recommendationsForLoggedInUser,
 	isRecommendationPageLoading,
 }: {
 	loggedInUser: User | undefined;
@@ -93,21 +96,112 @@ function getLoggedInUserStatus({
 	recommendationsForLoggedInUser: Recommendation[];
 	isRecommendationPageLoading: boolean;
 }): LoggedInUserStatus {
-	const tasks: LoggedInUserStatus['tasks'] = [];
-	let state: UserExperienceState = 'registered';
+	const isLoggedInUserStatusLoading =
+		loggedInUser == null ||
+		[
+			isLoggedInUserLoading,
+			isBankAccountPageLoading,
+			isLicensePageLoading,
+			isOrderPageLoading,
+			isPositionPageLoading,
+			isRecommendationPageLoading,
+		].some(Boolean);
 
-	const isLoggedInUserStatusLoading = [
-		isLoggedInUserLoading,
-		isBankAccountPageLoading,
-		isLicensePageLoading,
-		isOrderPageLoading,
-		isPositionPageLoading,
-		isRecommendationPageLoading,
-	].some(Boolean);
-
-	return {
+	const status: LoggedInUserStatus = {
 		isLoggedInUserStatusLoading,
-		state,
-		tasks,
+		state: 'registered',
+		tasks: [],
 	};
+
+	if (isLoggedInUserStatusLoading) {
+		return status;
+	}
+
+	const defaultRouteOptions = {
+		includeOrigin: false,
+		origin: null,
+		queryParams: {},
+	};
+
+	if (!isActivatedUser(loggedInUser)) {
+		status.tasks.push({
+			ctaHref: getHomeSiteRoute({
+				...defaultRouteOptions,
+				routeStaticId: 'HOME_SITE__/GET_STARTED',
+			}),
+			ctaText: 'Get Started',
+			subtitle:
+				"Activate your account to receive personalized AI insights and unlock all of Wallot's features.",
+			title: 'Activate Account',
+		});
+		return status;
+	}
+
+	const firstOrder = ordersForLoggedInUser[0];
+	if (firstOrder == null) return status;
+
+	if (!isKycUser(loggedInUser)) {
+		status.state = 'activated.inputting_kyc_and_bank';
+		status.tasks.push({
+			ctaHref: getHomeSiteRoute({
+				...defaultRouteOptions,
+				queryParams: { order_id: firstOrder._id },
+				routeStaticId: 'HOME_SITE__/ORDERS/[ORDER_ID]/CHECKOUT',
+			}),
+			ctaText: 'Continue',
+			subtitle:
+				"You're one step away from investing. Complete our billing information form, and add a bank to finish your order.",
+			title: 'Complete your first order',
+		});
+		return status;
+	}
+
+	const firstBankAccount = bankAccountsForLoggedInUser[0];
+
+	if (firstBankAccount == null || !isBankAccountTokenized(firstBankAccount)) {
+		status.state = 'activated.inputting_kyc_and_bank';
+		status.tasks.push({
+			ctaHref: getHomeSiteRoute({
+				...defaultRouteOptions,
+				queryParams: { order_id: firstOrder._id },
+				routeStaticId: 'HOME_SITE__/ORDERS/[ORDER_ID]/CHECKOUT',
+			}),
+			ctaText: 'Continue',
+			subtitle:
+				firstBankAccount == null
+					? 'Looks like you still need to connect a bank account. We use Stripe to make this process secure and easy.'
+					: 'Looks like you still need to confirm your bank account details. Head over to our checkout page to complete this step.',
+			title: 'Connect a bank account',
+		});
+		return status;
+	}
+
+	if (!isOrderConfirmedByUser(firstOrder)) {
+		status.state = 'activated.ready_to_confirm_order';
+		status.tasks.push({
+			ctaHref: getHomeSiteRoute({
+				...defaultRouteOptions,
+				queryParams: { order_id: firstOrder._id },
+				routeStaticId: 'HOME_SITE__/ORDERS/[ORDER_ID]/CHECKOUT',
+			}),
+			ctaText: 'Continue',
+			subtitle:
+				"Don't miss out on your investment. Confirm your order, and we'll take care of the rest.",
+			title: 'Complete your first order',
+		});
+		return status;
+	}
+
+	// TODO -- Add logic for the following states:
+	// | 'trackingProgress.waitingForOrderToBeFilled.waitingForAlpacaAccountToChangeFromSubmittedToActive'
+	// | 'trackingProgress.waitingForOrderToBeFilled.waitingForAlpacaAchRelationshipToChangeFromQueuedToApproved'
+	// | 'trackingProgress.waitingForOrderToBeFilled.waitingForAlpacaAchTransferToChangeFromQueuedToComplete'
+	// | 'trackingProgress.waitingForOrderToBeFilled.waitingForAlpacaOrderToChangeFromPendingNewToFilled'
+	// | 'trackingProgress.resolvingProblemWithOrder.resolvingAlpacaAccountActivationError'
+	// | 'trackingProgress.resolvingProblemWithOrder.resolvingAlpacaAchRelationshipError'
+	// | 'trackingProgress.resolvingProblemWithOrder.resolvingAlpacaAchTransferError'
+	// | 'trackingProgress.resolvingProblemWithOrder.resolvingAlpacaOrderError'
+
+	status.state = 'trackingProgress.homeostasis';
+	return status;
 }
