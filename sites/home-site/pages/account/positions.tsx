bf1de162393eb4c11c9747659de2e84efb00a78b
Kamar <kamarmack.home@gmail.com>
2024-12-29 11:17:14 +0000

    feat: add account skeleton pages for plans, banking, positions, statements, and settings

---
+++ sites/home-site/pages/account/positions.tsx
@@ -0,0 +1,80 @@
+import type { GetStaticProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import {
+	Page as PageComponent,
+	PageStaticProps,
+	PageProps,
+} from 'ergonomic-react/src/components/nextjs-pages/Page';
+import { HomeSiteRouteQueryParams, getSsoSiteRoute } from '@wallot/js';
+import { useSiteOriginByTarget } from '@wallot/react/src/hooks/useSiteOriginByTarget';
+import { useAuthenticatedRouteRedirect } from 'ergonomic-react/src/features/authentication/hooks/useAuthenticatedRouteRedirect';
+import { AccountDashboardPage } from '@wallot/home-site/src/components/AccountDashboardPage';
+
+const Page: NextPage<PageStaticProps> = (props) => {
+	// ==== Hooks ==== //
+
+	// Site origins
+	const siteOriginByTarget = useSiteOriginByTarget();
+
+	// Auth
+	useAuthenticatedRouteRedirect({
+		authSiteOrigin: siteOriginByTarget.SSO_SITE,
+		loginRoutePath: getSsoSiteRoute({
+			includeOrigin: false,
+			origin: null,
+			queryParams: {},
+			routeStaticId: 'SSO_SITE__/LOGIN',
+		}),
+		shouldPauseFirebaseAuthRedirects: false,
+	});
+
+	// Router
+	const router = useRouter();
+
+	// ==== Constants ==== //
+
+	// Router Query
+	const _: RouteQueryParams = router?.query ?? {};
+	_;
+
+	// ==== Constants ==== //
+
+	// Runtime Route ID
+	const ROUTE_RUNTIME_ID = props.routeStaticId;
+
+	// Runtime Page Props
+	const pageProps: PageProps = {
+		...props,
+		routeId: ROUTE_RUNTIME_ID,
+	};
+
+	// ==== Render ==== //
+	return (
+		<PageComponent {...pageProps}>
+			<AccountDashboardPage>
+				<div>Here are your positions!</div>
+			</AccountDashboardPage>
+		</PageComponent>
+	);
+};
+
+export default Page;
+
+// ==== Static Page Props ==== //
+
+// Route Static ID
+const ROUTE_STATIC_ID = 'HOME_SITE__/ACCOUNT/POSITIONS' as const;
+
+// Route Query Params Type
+type RouteQueryParams = HomeSiteRouteQueryParams[typeof ROUTE_STATIC_ID];
+
+export const getStaticProps: GetStaticProps<PageStaticProps> = () => {
+	// Route Static Props
+	const ROUTE_STATIC_PROPS: PageStaticProps = {
+		routeStaticId: ROUTE_STATIC_ID,
+		title: 'Positions',
+	};
+	return Promise.resolve({
+		props: ROUTE_STATIC_PROPS,
+	});
+};
