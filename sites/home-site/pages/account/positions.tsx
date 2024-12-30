8dc3abbc06220d842e257cca8201294ec53af97e
Kamar <kamarmack.home@gmail.com>
2024-12-30 14:35:13 +0000

    Close #68

--- sites/home-site/pages/account/positions.tsx
+++ sites/home-site/pages/account/positions.tsx
@@ -1,3 +1,4 @@
+import * as changeCase from 'change-case';
 import type { GetStaticProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import {
@@ -8,6 +9,8 @@ import {
 import { HomeSiteRouteQueryParams } from '@wallot/js';
 import { AccountDashboardPage } from '@wallot/home-site/src/components/AccountDashboardPage';
 import { useRetrievePositions } from '@wallot/react/src/features/positions';
+import { default as cn } from 'ergonomic-react/src/lib/cn';
+import { getCurrencyUsdStringFromCents } from 'ergonomic';
 
 const Page: NextPage<PageStaticProps> = (props) => {
 	// ==== Hooks ==== //
@@ -39,19 +42,53 @@ const Page: NextPage<PageStaticProps> = (props) => {
 	// ==== Render ==== //
 	return (
 		<PageComponent {...pageProps}>
-			<AccountDashboardPage>
-				<div>Here are your positions!</div>
+			<AccountDashboardPage className={cn('lg:max-w-3xl')}>
 				<div>
-					{positions.map(({ avg_entry_price, qty, side, symbol }) => {
-						return (
-							<div key={symbol}>
-								<div>{symbol}</div>
-								<div>{qty}</div>
-								<div>{avg_entry_price}</div>
-								<div>{side}</div>
-							</div>
-						);
-					})}
+					<div>
+						<p className='font-semibold text-2xl'>Positions</p>
+					</div>
+					<div className='mt-1'>
+						<p className='font-light text-base text-gray-600'>
+							Below are the current positions in your account.
+						</p>
+					</div>
+					<div className='mt-3 border-b border-gray-200 py-1'>
+						<div className='grid grid-cols-4 gap-4'>
+							{['Symbol', 'Shares', 'Avg Entry Price', 'Side'].map((header) => {
+								return (
+									<div key={header}>
+										<p className='font-semibold text-base text-gray-600'>
+											{header}
+										</p>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+					<div className='mt-2 flex flex-col gap-2'>
+						{positions.map(({ avg_entry_price, qty, side, symbol }) => {
+							const avgEntryPriceCents =
+								avg_entry_price == null ? 0 : parseFloat(avg_entry_price) * 100;
+							const avgEntryPriceUsdString =
+								getCurrencyUsdStringFromCents(avgEntryPriceCents);
+							return (
+								<div key={symbol} className={cn('grid grid-cols-4 gap-4')}>
+									{[
+										symbol,
+										qty,
+										avgEntryPriceUsdString,
+										changeCase.capitalCase(side ?? ''),
+									].map((value, valueIdx) => {
+										return (
+											<div key={valueIdx}>
+												<p className='font-light text-sm'>{value}</p>
+											</div>
+										);
+									})}
+								</div>
+							);
+						})}
+					</div>
 				</div>
 			</AccountDashboardPage>
 		</PageComponent>
