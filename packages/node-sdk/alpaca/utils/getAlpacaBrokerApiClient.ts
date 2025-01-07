6be6660ce02066f629938237bac3377b1dd98d6b
Kamar <kamarmack.home@gmail.com>
2025-01-07 15:48:52 +0000

    Add Alpaca Broker Estimation API Base URL to environment configurations Close #171

--- packages/node-sdk/alpaca/utils/getAlpacaBrokerApiClient.ts
+++ packages/node-sdk/alpaca/utils/getAlpacaBrokerApiClient.ts
@@ -45,12 +45,12 @@ export const getAlpacaBrokerApiClient = ({
 	});
 
 export const getAlpacaBrokerEstimationApiClient = ({
-	SECRET_CRED_ALPACA_BROKER_API_BASE_URL,
+	SECRET_CRED_ALPACA_BROKER_ESTIMATION_API_BASE_URL,
 	SECRET_CRED_ALPACA_BROKER_ESTIMATION_API_KEY,
 	SECRET_CRED_ALPACA_BROKER_ESTIMATION_API_SECRET,
 }: SecretData) =>
 	ky.create({
-		prefixUrl: SECRET_CRED_ALPACA_BROKER_API_BASE_URL,
+		prefixUrl: SECRET_CRED_ALPACA_BROKER_ESTIMATION_API_BASE_URL,
 		headers: {
 			accept: 'application/json',
 			authorization: `Basic ${Buffer.from(
