c47605d139cb6eeca214245b88ba0f9a132334a7
Kamar <kamarmack.home@gmail.com>
2025-01-12 06:33:37 +0000

    Add daysAfterEntry parameter to deriveNetGainForAssetOrder function

--- functions/rest-api/lib/utils/deriveNetGainForAssetOrder.ts
+++ functions/rest-api/lib/utils/deriveNetGainForAssetOrder.ts
@@ -2,6 +2,7 @@ import { AssetOrder } from '@wallot/js';
 
 export const deriveNetGainForAssetOrder = async (
 	assetOrder: AssetOrder,
+	daysAfterEntry = 30,
 ): Promise<number> => {
 	throw new Error('Not implemented');
 };
