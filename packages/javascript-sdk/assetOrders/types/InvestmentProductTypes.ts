0c13faf5ee35b70c5b4775a93e3d4c6b214cf185
Kamar <kamarmack.home@gmail.com>
2025-01-12 07:34:13 +0000

    Add InvestmentProduct and InvestmentProductNetGain types with related model imports

--- packages/javascript-sdk/assetOrders/types/InvestmentProductTypes.ts
+++ packages/javascript-sdk/assetOrders/types/InvestmentProductTypes.ts
@@ -1,3 +1,8 @@
+import { Model } from '../../models/models/modelProperties.js';
+import { ModelFamily } from '../../modelFamilies/models/modelFamilyProperties.js';
+import { OpenAiModel } from '../../openAiModels/models/openAiModelProperties.js';
+import { OpenAiModelFamily } from '../../openAiModelFamilies/models/openAiModelFamilyProperties.js';
+
 export type Trade = {
 	amount: string; // String number of dollars, e.g. '1000' for $1,000.00
 	date: string; // UTC date, e.g. '2024-12-21T01:11:14.887Z'
@@ -18,4 +23,25 @@ export type TradeNetGainResults = {
 export type TradeNetGain = {
 	trade: Trade;
 	results: TradeNetGainResults;
+};
+
+export type InvestmentProduct = {
+	entry_date: string;
+	model: {
+		financial_model: Model;
+		financial_model_family: ModelFamily;
+		llm: OpenAiModel;
+		llm_family: OpenAiModelFamily;
+	};
+	title: string;
+	trades: Trade[];
+};
+
+export type InvestmentProductNetGainResults = Omit<
+	TradeNetGainResults,
+	'num_shares'
+>;
+export type InvestmentProductNetGain = {
+	investment_product: InvestmentProduct;
+	results: InvestmentProductNetGainResults;
 };
