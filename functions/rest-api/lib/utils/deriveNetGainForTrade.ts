c34b5a81ff21219d70101094a759edc0e53b8d28
Kamar <kamarmack.home@gmail.com>
2025-01-12 07:09:29 +0000

    Complete deriveNetGainForTrade implementation Close #214

--- functions/rest-api/lib/utils/deriveNetGainForTrade.ts
+++ functions/rest-api/lib/utils/deriveNetGainForTrade.ts
@@ -1,21 +1,50 @@
 import { getFutureDate, getNewYorkDate } from '@wallot/js';
 import { retrieveAssetPrice } from './retrieveAssetPrice.js';
+import { log } from '../services.js';
 
 export const deriveNetGainForTrade = async (
 	trade: Trade,
 	daysAfterEntry = 30,
-): Promise<number> => {
+): Promise<NetGain> => {
 	const entryDate = getNewYorkDate(trade.date);
+	const exitDate = getFutureDate(entryDate, daysAfterEntry);
 	const entryAssetPriceParams: [string, string] = [trade.symbol, entryDate];
-	const exitAssetPriceParams: [string, string] = [
-		trade.symbol,
-		getFutureDate(entryDate, daysAfterEntry),
-	];
-	const [entryAssetPrice, exitAssetPrice] = await Promise.all([
+	const exitAssetPriceParams: [string, string] = [trade.symbol, exitDate];
+	const [{ close: entryClose }, { close: exitClose }] = await Promise.all([
 		retrieveAssetPrice(entryAssetPriceParams),
 		retrieveAssetPrice(exitAssetPriceParams),
 	]);
-	throw new Error('Not implemented');
+
+	if (entryClose == null || exitClose == null) {
+		const message = 'Fix the asset price data in the database.';
+		log(
+			{
+				code: 'MISSING_CLOSE_PRICE_IN_ASSET_PRICE_ERROR',
+				message,
+				trade,
+				daysAfterEntry,
+				entryDate,
+				exitDate,
+			},
+			{ type: 'error' },
+		);
+		throw new Error(message);
+	}
+
+	const entryNumShares = getNumShares(trade.amount, entryClose);
+	const entryTotalPrice = entryNumShares * parseFloat(entryClose);
+	const exitTotalPrice = entryNumShares * parseFloat(exitClose);
+	const netGain = exitTotalPrice - entryTotalPrice;
+	const netGainRate = netGain / entryTotalPrice;
+	return {
+		num_shares: entryNumShares,
+		entry_date: entryDate,
+		entry_price: parseFloat(entryClose),
+		exit_date: exitDate,
+		exit_price: parseFloat(exitClose),
+		net_gain: netGain,
+		net_gain_rate: netGainRate,
+	};
 };
 
 type Trade = {
@@ -23,3 +52,18 @@ type Trade = {
 	date: string; // UTC date, e.g. '2024-12-21T01:11:14.887Z'
 	symbol: string;
 };
+type NetGain = {
+	num_shares: number;
+	entry_date: string;
+	entry_price: number;
+	exit_date: string;
+	exit_price: number;
+	net_gain: number;
+	net_gain_rate: number;
+};
+
+function getNumShares(amount: string, price: string): number {
+	const amountNum = parseFloat(amount);
+	const priceNum = parseFloat(price);
+	return amountNum / priceNum;
+}
