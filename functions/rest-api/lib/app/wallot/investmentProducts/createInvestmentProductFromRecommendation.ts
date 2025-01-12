577482e10711670d5a09726e7d68e43897e00b86
Kamar <kamarmack.home@gmail.com>
2025-01-12 12:58:51 +0000

    Refactor createInvestmentProductFromRecommendation to utilize caching for financial models and OpenAI models

--- functions/rest-api/lib/app/wallot/investmentProducts/createInvestmentProductFromRecommendation.ts
+++ functions/rest-api/lib/app/wallot/investmentProducts/createInvestmentProductFromRecommendation.ts
@@ -12,6 +12,8 @@ import {
 	OpenAiModelFamily,
 	openAiModelFamiliesApi,
 	getNewYorkDate,
+	Model,
+	ModelFamily,
 } from '@wallot/js';
 import { db } from '../../../services.js';
 
@@ -23,6 +25,12 @@ const openAiModelFamiliesCollectionId = openAiModelFamiliesApi.collectionId;
 
 export const createInvestmentProductFromRecommendation = async (
 	recommendation: Recommendation,
+	cache: {
+		model_family: ModelFamily[];
+		model: Model[];
+		open_ai_model_family: OpenAiModelFamily[];
+		open_ai_model: OpenAiModel[];
+	} | null = null,
 ): Promise<InvestmentProduct> => {
 	const {
 		_date_created,
@@ -33,48 +41,76 @@ export const createInvestmentProductFromRecommendation = async (
 	} = recommendation;
 	const entryDate = getNewYorkDate(_date_created);
 
-	// Query the financial model
-	const financialModelSnapshot = await db
-		.collection(financialModelsCollectionId)
-		.doc(financialModelId)
-		.get();
-	if (!financialModelSnapshot.exists) {
-		throw new Error('Financial model not found');
+	// Query from cache
+	let financialModel =
+		cache?.model?.find((model) => model._id === financialModelId) ?? null;
+	let financialModelFamily =
+		cache?.model_family?.find((family) =>
+			financialModel == null
+				? false
+				: family._id === financialModel.model_family,
+		) ?? null;
+	let llm =
+		cache?.open_ai_model?.find((model) =>
+			financialModel == null
+				? false
+				: model._id === financialModel?.open_ai_model,
+		) ?? null;
+	let llmFamily =
+		cache?.open_ai_model_family?.find((family) =>
+			llm == null ? false : family._id === llm.open_ai_model_family,
+		) ?? null;
+
+	if (financialModel == null) {
+		// Query the financial model
+		const financialModelSnapshot = await db
+			.collection(financialModelsCollectionId)
+			.doc(financialModelId)
+			.get();
+		if (!financialModelSnapshot.exists) {
+			throw new Error('Financial model not found');
+		}
+		financialModel = financialModelSnapshot.data() as FinancialModel;
 	}
-	const financialModel = financialModelSnapshot.data() as FinancialModel;
 	const { model_family: financialModelFamilyId, open_ai_model: openAiModelId } =
 		financialModel;
 
-	// Concurrently query the financial model family and the OpenAI model
-	const [financialModelFamilySnapshot, openAiModelSnapshot] = await Promise.all(
-		[
-			db
-				.collection(financialModelFamiliesCollectionId)
-				.doc(financialModelFamilyId)
-				.get(),
-			db.collection(openAiModelsCollectionId).doc(openAiModelId).get(),
-		],
-	);
-	if (!financialModelFamilySnapshot.exists) {
-		throw new Error('Financial model family not found');
+	if (financialModelFamily == null || openAiModelId == null) {
+		// Concurrently query the financial model family and the OpenAI model
+		const [financialModelFamilySnapshot, openAiModelSnapshot] =
+			await Promise.all([
+				db
+					.collection(financialModelFamiliesCollectionId)
+					.doc(financialModelFamilyId)
+					.get(),
+				db.collection(openAiModelsCollectionId).doc(openAiModelId).get(),
+			]);
+		if (!financialModelFamilySnapshot.exists) {
+			throw new Error('Financial model family not found');
+		}
+		if (!openAiModelSnapshot.exists) {
+			throw new Error('OpenAI model not found');
+		}
+		financialModelFamily =
+			financialModelFamilySnapshot.data() as FinancialModelFamily;
+		llm = openAiModelSnapshot.data() as OpenAiModel;
 	}
-	if (!openAiModelSnapshot.exists) {
+	if (llm == null) {
 		throw new Error('OpenAI model not found');
 	}
-	const financialModelFamily =
-		financialModelFamilySnapshot.data() as FinancialModelFamily;
-	const llm = openAiModelSnapshot.data() as OpenAiModel;
 	const { open_ai_model_family: llmFamilyId } = llm;
 
-	// Query the OpenAI model family
-	const llmFamilySnapshot = await db
-		.collection(openAiModelFamiliesCollectionId)
-		.doc(llmFamilyId)
-		.get();
-	if (!llmFamilySnapshot.exists) {
-		throw new Error('OpenAI model family not found');
+	if (llmFamily == null) {
+		// Query the OpenAI model family
+		const llmFamilySnapshot = await db
+			.collection(openAiModelFamiliesCollectionId)
+			.doc(llmFamilyId)
+			.get();
+		if (!llmFamilySnapshot.exists) {
+			throw new Error('OpenAI model family not found');
+		}
+		llmFamily = llmFamilySnapshot.data() as OpenAiModelFamily;
 	}
-	const llmFamily = llmFamilySnapshot.data() as OpenAiModelFamily;
 
 	// Construct the investment product
 	const investmentProductId = v4();
