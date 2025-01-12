ebd3584a33340566a1762ea542fff7d17e2c4d04
Kamar <kamarmack.home@gmail.com>
2025-01-12 05:40:01 +0000

    Implement retrieveAssetPrice

--- functions/rest-api/lib/utils/retrieveAssetPrice.ts
+++ functions/rest-api/lib/utils/retrieveAssetPrice.ts
@@ -3,29 +3,32 @@ import {
 	assetPricesApi,
 	getAssetPriceDocumentName,
 } from '@wallot/js';
-import { alphaVantage, db, log } from '../services.js';
+import { db, log } from '../services.js';
 
 export const retrieveAssetPrice = async ([symbol, timestamp]: Parameters<
 	typeof getAssetPriceDocumentName
 >): Promise<AssetPrice> => {
-	try {
-		// Query the database for the asset price
-		const name = getAssetPriceDocumentName(symbol, timestamp);
-		const assetPriceSnapshot = await db
-			.collection(assetPricesApi.collectionId)
-			.where('name', '==', name)
-			.get();
+	// Query the database for the asset price
+	const name = getAssetPriceDocumentName(symbol, timestamp);
+	const assetPriceSnapshot = await db
+		.collection(assetPricesApi.collectionId)
+		.where('name', '==', name)
+		.get();
 
-		// If the asset price doesn't exist, query the Alpha Vantage API
-		if (assetPriceSnapshot.empty || assetPriceSnapshot.docs[0] == null) {
-			throw new Error('Alpha Vantage API not implemented');
-		}
-
-		// If the asset price exists already, return it
-		const assetPrice = assetPriceSnapshot.docs[0].data() as AssetPrice;
-		return assetPrice;
-	} catch (error) {
-		log({ error }, { type: 'error' });
-		throw new Error('Failed to retrieve asset price');
+	// If the asset price doesn't exist, throw an error
+	if (assetPriceSnapshot.empty || assetPriceSnapshot.docs[0] == null) {
+		const message = 'Query the Alpha Vantage API to get the asset price first.';
+		log(
+			{
+				code: 'MISSING_ASSET_PRICE_ERROR',
+				message,
+			},
+			{ type: 'error' },
+		);
+		throw new Error(message);
 	}
+
+	// If the asset price exists already, return it
+	const assetPrice = assetPriceSnapshot.docs[0].data() as AssetPrice;
+	return assetPrice;
 };
