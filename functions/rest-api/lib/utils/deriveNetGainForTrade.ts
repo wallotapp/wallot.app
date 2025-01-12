5a95e47cabec73b32d6f69db0f8b7a71052e3281
Kamar <kamarmack.home@gmail.com>
2025-01-12 06:39:25 +0000

    Refactor deriveNetGainForTrade to accept a Trade type instead of AssetOrder

--- functions/rest-api/lib/utils/deriveNetGainForTrade.ts
+++ functions/rest-api/lib/utils/deriveNetGainForTrade.ts
@@ -1,8 +1,12 @@
-import { AssetOrder } from '@wallot/js';
-
 export const deriveNetGainForTrade = async (
-	assetOrder: AssetOrder,
+	trade: Trade,
 	daysAfterEntry = 30,
 ): Promise<number> => {
 	throw new Error('Not implemented');
+};
+
+type Trade = {
+	amount: string; // String number of dollars, e.g. '1000' for $1,000.00
+	date: string; // UTC date, e.g. '2024-12-21T01:11:14.887Z'
+	symbol: string;
 };
