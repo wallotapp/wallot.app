dc18a606e3ac90cd7cb50a97c06c0fee3f334f9b
Kamar <kamarmack.home@gmail.com>
2025-01-12 06:32:08 +0000

    Add skeleton for deriveNetGainForAssetOrder

---
+++ functions/rest-api/lib/utils/deriveNetGainForAssetOrder.ts
@@ -0,0 +1,7 @@
+import { AssetOrder } from '@wallot/js';
+
+export const deriveNetGainForAssetOrder = async (
+	assetOrder: AssetOrder,
+): Promise<number> => {
+	throw new Error('Not implemented');
+};
