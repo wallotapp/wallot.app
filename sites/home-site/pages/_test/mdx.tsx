f74a5456675d9ebd320be27d35c24117870f46de
Kamar <kamarmack.home@gmail.com>
2025-01-14 02:22:17 +0000

    Fix MDX serialization by trimming front matter and improving content handling

--- sites/home-site/pages/_test/mdx.tsx
+++ sites/home-site/pages/_test/mdx.tsx
@@ -58,7 +58,9 @@ What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
 printing and typesetting industry. Lorem Ipsum has been the
 industry's standard dummy text ever since the 1500s, when an
 unknown printer took a galley of type and scrambled it to make a
-type specimen book. It has survived not only five centuries, but
+type specimen book. 
+
+It has survived not only five centuries, but
 also the leap into **electronic typesetting**, remaining essentially
 unchanged. It was popularised in the 1960s with the release of
 Letraset sheets containing Lorem Ipsum passages, and more recently
@@ -70,7 +72,7 @@ with desktop publishing software like Aldus.
 			title: 'Hello world, this is a blog!',
 		},
 	};
-	const mdxContent = await serialize(mdxFile.content, {
+	const mdxContent = await serialize(mdxFile.content.split('---').pop() ?? '', {
 		mdxOptions: {
 			remarkPlugins: [[mdxMermaid, { output: 'svg' }]],
 			rehypePlugins: [],
