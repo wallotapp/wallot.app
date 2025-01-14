95b5547b21319de658e8ff38b0a09ad72b34df8c
Kamar <kamarmack.home@gmail.com>
2025-01-14 02:35:47 +0000

    Add example code snippet for MonacoCodeSnippet component

--- packages/react-sdk/src/components/MonacoCodeSnippet.tsx
+++ packages/react-sdk/src/components/MonacoCodeSnippet.tsx
@@ -35,3 +35,24 @@ export const MonacoCodeSnippet: React.FC<MonacoCodeSnippetProps> = ({
 		<div ref={editorContainerRef} style={{ height }} className={className} />
 	);
 };
+
+export const exampleCodeSnippet = `import * as yup from 'yup';
+
+const schema = yup.object().shape({
+	name: yup.string().required(),
+	age: yup.number().required().positive().integer(),
+});
+
+const data = {
+	name: 'John Doe',
+	age: 30,
+};
+
+schema
+	.validate(data)
+	.then(() => {
+		console.log('Data is valid');
+	})
+	.catch((err) => {
+		console.log(err.errors);
+	});`;
