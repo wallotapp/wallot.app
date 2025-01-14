c77e073d8d369d4128f852ea50edabb797031fb4
Kamar <kamarmack.home@gmail.com>
2025-01-14 02:34:50 +0000

    Refactor getMDXComponents to simplify component mapping

--- packages/react-sdk/src/components/getMDXComponents.tsx
+++ packages/react-sdk/src/components/getMDXComponents.tsx
@@ -13,20 +13,20 @@ type GetMDXComponentProps = {
 	};
 };
 export function getMDXComponents(_: GetMDXComponentProps) {
-	const components: Record<string, T> = {
-		BlockMath: BlockMath as T,
-		ExLink: ExLink as T,
-		Image: Image as unknown as T,
-		InlineMath: InlineMath as T,
-		Mermaid: Mermaid as T,
-		MonacoCodeSnippet: MonacoCodeSnippet as T,
-		h1: ((props: U) => <Heading level={1} {...props} />) as T,
-		h2: ((props: U) => <Heading level={2} {...props} />) as T,
-		h3: ((props: U) => <Heading level={3} {...props} />) as T,
-		h4: ((props: U) => <Heading level={4} {...props} />) as T,
-		h5: ((props: U) => <Heading level={5} {...props} />) as T,
-		h6: ((props: U) => <Heading level={6} {...props} />) as T,
-	};
+	const components = {
+		BlockMath,
+		ExLink,
+		Image,
+		InlineMath,
+		Mermaid,
+		MonacoCodeSnippet,
+		h1: (props: U) => <Heading level={1} {...props} />,
+		h2: (props: U) => <Heading level={2} {...props} />,
+		h3: (props: U) => <Heading level={3} {...props} />,
+		h4: (props: U) => <Heading level={4} {...props} />,
+		h5: (props: U) => <Heading level={5} {...props} />,
+		h6: (props: U) => <Heading level={6} {...props} />,
+	} as unknown as Record<string, T>;
 	return components;
 }
 
