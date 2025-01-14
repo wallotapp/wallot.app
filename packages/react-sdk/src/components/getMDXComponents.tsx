d607e111ab61af73351d281061aeb4798bcca2c7
Kamar <kamarmack.home@gmail.com>
2025-01-14 02:14:27 +0000

    Add Heading component; Add getMDXComponents

---
+++ packages/react-sdk/src/components/getMDXComponents.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import { Mermaid } from 'mdx-mermaid/lib/Mermaid';
+import { InlineMath, BlockMath } from 'react-katex';
+import { Heading } from '@wallot/react/src/components/Heading';
+
+type GetMDXComponentProps = {
+	frontMatter: {
+		date_published: string;
+		parent: string;
+		title: string;
+	};
+};
+export function getMDXComponents(_: GetMDXComponentProps) {
+	const components: {
+		[key: string]: T;
+	} = {
+		BlockMath: BlockMath as T,
+		Image: Image as unknown as T,
+		InlineMath: InlineMath as T,
+		Mermaid: Mermaid as T,
+		h1: ((props: U) => <Heading level={1} {...props} />) as T,
+		h2: ((props: U) => <Heading level={2} {...props} />) as T,
+		h3: ((props: U) => <Heading level={3} {...props} />) as T,
+		h4: ((props: U) => <Heading level={4} {...props} />) as T,
+		h5: ((props: U) => <Heading level={5} {...props} />) as T,
+		h6: ((props: U) => <Heading level={6} {...props} />) as T,
+	};
+	return components;
+}
+
+type T = () => JSX.Element;
+type U = { children: string };
