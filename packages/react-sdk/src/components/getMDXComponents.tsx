fd80c181f8a67e7bc13bd765efaf90bac18b8d44
Kamar <kamarmack.home@gmail.com>
2025-01-14 02:30:03 +0000

    fix format

--- packages/react-sdk/src/components/getMDXComponents.tsx
+++ packages/react-sdk/src/components/getMDXComponents.tsx
@@ -14,7 +14,7 @@ type GetMDXComponentProps = {
 export function getMDXComponents(_: GetMDXComponentProps) {
 	const components: Record<string, T> = {
 		BlockMath: BlockMath as T,
-    ExLink: ExLink as T,
+		ExLink: ExLink as T,
 		Image: Image as unknown as T,
 		InlineMath: InlineMath as T,
 		Mermaid: Mermaid as T,
