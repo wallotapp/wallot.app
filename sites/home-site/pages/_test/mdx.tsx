e586b3adb12afcd3ab5e51200aebe830db3cc2e9
Kamar <kamarmack.home@gmail.com>
2025-01-14 02:14:56 +0000

    Add MDX support with Prose component and static props for blog content

--- sites/home-site/pages/_test/mdx.tsx
+++ sites/home-site/pages/_test/mdx.tsx
@@ -1,8 +1,21 @@
 import type { NextPage } from 'next';
 import { Fragment } from 'react';
 import { useIsMounted } from 'ergonomic-react/src/hooks/useIsMounted';
+import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote';
+import { default as mdxMermaid } from 'mdx-mermaid';
+import { serialize } from 'next-mdx-remote/serialize';
+import { Prose } from '@wallot/react/src/components/Prose';
+import { getMDXComponents } from '@wallot/react/src/components/getMDXComponents';
 
-const Page: NextPage = () => {
+type PostPageProps = {
+	frontMatter: {
+		date_published: string;
+		parent: string;
+		title: string;
+	};
+	mdx: Omit<MDXRemoteProps, 'components'>;
+};
+const Page: NextPage<PostPageProps> = ({ frontMatter, mdx }) => {
 	const isMounted = useIsMounted();
 
 	if (!isMounted) {
@@ -15,6 +28,14 @@ const Page: NextPage = () => {
 				<div>
 					<p>Here's some MDX</p>
 				</div>
+				<div>
+					<Prose>
+						<MDXRemote
+							{...mdx}
+							components={getMDXComponents({ frontMatter })}
+						/>
+					</Prose>
+				</div>
 				<div>Let us know if you like it!</div>
 			</div>
 		</Fragment>
@@ -22,3 +43,45 @@ const Page: NextPage = () => {
 };
 
 export default Page;
+
+export const getStaticProps = async () => {
+	const mdxFile = {
+		content: `---
+date_published: "2025-01-13T00:00:00.000Z"
+parent: ""
+title: "Hello world, this is a blog!"
+---
+
+# Hello world, this is a blog!
+
+What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+printing and typesetting industry. Lorem Ipsum has been the
+industry's standard dummy text ever since the 1500s, when an
+unknown printer took a galley of type and scrambled it to make a
+type specimen book. It has survived not only five centuries, but
+also the leap into **electronic typesetting**, remaining essentially
+unchanged. It was popularised in the 1960s with the release of
+Letraset sheets containing Lorem Ipsum passages, and more recently
+with desktop publishing software like Aldus.
+`,
+		frontMatter: {
+			date_published: '2025-01-13T00:00:00.000Z',
+			parent: '',
+			title: 'Hello world, this is a blog!',
+		},
+	};
+	const mdxContent = await serialize(mdxFile.content, {
+		mdxOptions: {
+			remarkPlugins: [[mdxMermaid, { output: 'svg' }]],
+			rehypePlugins: [],
+		},
+		scope: mdxFile.frontMatter,
+	});
+	const props = {
+		frontMatter: mdxFile.frontMatter,
+		mdx: mdxContent,
+	};
+	return {
+		props,
+	};
+};
