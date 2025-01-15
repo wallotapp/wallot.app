0eaaf86ff29f39550d2f8463026cf26428fd131a
Kamar <kamarmack.home@gmail.com>
2025-01-15 02:02:15 +0000

    Add MDXPage component to React SDK

--- sites/blog-site/pages/article/[slug].tsx
+++ sites/blog-site/pages/article/[slug].tsx
@@ -14,12 +14,7 @@ import {
 } from '@wallot/react/src/types/MDXTypes';
 import { default as mdxMermaid } from 'mdx-mermaid';
 import { serialize } from 'next-mdx-remote/serialize';
-import { Prose } from '@wallot/react/src/components/Prose';
-import { getMDXComponents } from '@wallot/react/src/components/getMDXComponents';
-import { MDXRemote } from 'next-mdx-remote';
-import { default as cn } from 'ergonomic-react/src/lib/cn';
-import { PageHeader } from '@wallot/react/src/components/PageHeader';
-import { Heading } from '@wallot/react/src/components/Heading';
+import { MDXPage } from '@wallot/react/src/components/MDXPage';
 
 // ==== Static Page Props ==== //
 
@@ -62,25 +57,7 @@ const Page: NextPage<MDXPageProps> = ({ mdx }) => {
 	// ==== Render ==== //
 	return (
 		<PageComponent {...pageProps}>
-			<div className={cn('flex flex-col min-h-screen min-w-screen relative')}>
-				<PageHeader />
-				<div
-					className={cn(
-						'min-h-[95vh] w-full scroll-smooth',
-						'py-28 px-6',
-						'lg:py-28 lg:px-28',
-					)}
-				>
-					<div className='lg:max-w-3xl lg:mx-auto'>
-						<Prose>
-							<div className='text-center mb-7'>
-								<Heading level={1}>{mdx.scope.title}</Heading>
-							</div>
-							<MDXRemote {...mdx} components={getMDXComponents(mdx.scope)} />
-						</Prose>
-					</div>
-				</div>
-			</div>
+			<MDXPage mdx={mdx} />
 		</PageComponent>
 	);
 };
