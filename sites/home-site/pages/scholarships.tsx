328c3c17000757f2ec9a9e2405a3d73d2234572f
Kamar <kamarmack.home@gmail.com>
2025-02-11 21:09:25 +0000

    Fill in Scholarships page with application status handling, updated UI elements and copy edit

--- sites/home-site/pages/scholarships.tsx
+++ sites/home-site/pages/scholarships.tsx
@@ -5,7 +5,11 @@ import {
 	PageProps,
 	Page as PageComponent,
 } from 'ergonomic-react/src/components/nextjs-pages/Page';
-import { getHomeSiteRoute, HomeSiteRouteQueryParams } from '@wallot/js';
+import {
+	getHomeSiteRoute,
+	HomeSiteRouteQueryParams,
+	ScholarshipApplication,
+} from '@wallot/js';
 import { Fragment } from 'react';
 import { default as cn } from 'ergonomic-react/src/lib/cn';
 import Link from 'next/link';
@@ -20,15 +24,9 @@ const Page: NextPage<PageProps> = (props) => {
 
 	// Site Origin
 	const siteOriginByTarget = useSiteOriginByTarget();
+	const homeSiteOrigin = siteOriginByTarget['HOME_SITE'];
 
 	// ==== Constants ==== //
-	const homeSiteOrigin = siteOriginByTarget['HOME_SITE'];
-	const applicationUrl = getHomeSiteRoute({
-		includeOrigin: true,
-		origin: homeSiteOrigin,
-		queryParams: {},
-		routeStaticId: 'HOME_SITE__/SCHOLARSHIPS',
-	});
 
 	// Router Query
 	const query: RouteQueryParams = router?.query ?? {};
@@ -36,6 +34,20 @@ const Page: NextPage<PageProps> = (props) => {
 	// Router Query Param Values
 	const _ = query;
 	typeof _;
+
+	// Application status
+	const applicationForLoggedInUser = null as ScholarshipApplication | null;
+	const isApplicationForLoggedInUserSubmitted = isApplicationSubmitted(
+		applicationForLoggedInUser,
+	);
+
+	// Application URL
+	const applicationUrl = getHomeSiteRoute({
+		includeOrigin: true,
+		origin: homeSiteOrigin,
+		queryParams: {},
+		routeStaticId: 'HOME_SITE__/SCHOLARSHIPS',
+	});
 
 	// ==== Constants ==== //
 
@@ -52,52 +64,49 @@ const Page: NextPage<PageProps> = (props) => {
 	return (
 		<PageComponent {...pageProps}>
 			<Fragment>
-				<div className='p-6'>
+				<div className='pt-6 px-6 bg-black'>
 					<div className='flex items-center justify-between'>
 						<div className='flex items-center space-x-2'>
-							<LogoButton />
+							<LogoButton theme='dark' />
 						</div>
 						<Link
 							className='hidden lg:block'
-							href={applicationUrl}
+							href='mailto:scholarships@wallot.app'
 							target='_blank'
 						>
-							<div
-								className={cn(
-									'bg-black hover:bg-brand-dark',
-									'font-medium inline-block px-6 py-1.5 rounded text-white',
-									'transition-colors',
-								)}
-							>
-								Apply
+							<div className={cn('font-extralight p-2 text-white')}>
+								Contact Us
 							</div>
 						</Link>
 					</div>
-					<div className='h-56' />
-					<p className='text-5xl font-normal mb-1'>
+					<div className='h-36' />
+					<p className='text-5xl font-normal text-gray-100 text-center'>
 						Florida Visionary Scholarship
 					</p>
-					<p className='text-xl text-gray-500 font-extralight'>
+					<p className='text-xl text-gray-400 font-extralight text-center mt-1.5'>
 						$5,000 Merit Scholarships for Graduating Seniors from Florida
 					</p>
-					<Link
-						className='block lg:hidden mt-4'
-						href={applicationUrl}
-						target='_blank'
-					>
-						<div
-							className={cn(
-								'bg-black hover:bg-green-500 hover:border-green-600',
-								'font-medium inline-block px-6 py-1.5 rounded text-white',
-								'transition-colors',
-							)}
-						>
-							Apply
-						</div>
-					</Link>
+					<div className='mt-5 text-center'>
+						<Link className='' href={applicationUrl} target='_blank'>
+							<div
+								className={cn(
+									'bg-gray-100 hover:bg-brand-dark',
+									'font-medium inline-block px-6 py-1.5 rounded text-black hover:text-white',
+									'transition-colors',
+								)}
+							>
+								{applicationForLoggedInUser == null
+									? 'Start'
+									: isApplicationForLoggedInUserSubmitted
+									? 'View'
+									: 'Continue'}{' '}
+								Application
+							</div>
+						</Link>
+					</div>
+					<div className='h-36' />
 				</div>
-				<hr className='my-4' />
-				<div className='grid grid-cols-4 gap-8 p-6'>
+				<div className='grid grid-cols-4 gap-8 pb-6 pt-10 px-6'>
 					<div
 						className={cn(
 							'flex flex-col space-y-5 col-span-3',
@@ -140,62 +149,99 @@ const Page: NextPage<PageProps> = (props) => {
 					<div className='col-span-3'>
 						<div className='mb-6 font-extralight flex flex-col space-y-3'>
 							<p className={cn('text-2xl text-gray-800 font-semibold')}>
-								Heading 1
+								A Commitment to Florida's Future
 							</p>
 							<p>
 								<span className={cn('font-medium text-gray-800')}>
-									It has survived not only five centuries, but also the leap
-									into electronic typesetting.
+									Wallot is excited to announce that we are currently accepting
+									applications for our inaugural Florida Visionary Scholarship
+									Program.
 								</span>{' '}
 								<span className='font-extralight italic'>
-									It was popularised in the 1960s with the release of Letraset
+									This program celebrates the inspirational academic, athletic,
+									and extracurricular achievement of Florida students by
+									awarding each recipient a yearly merit scholarship toward
+									their college's cost of attendance.
 								</span>
+							</p>
+							<p>
+								All graduating Seniors from the state of Florida who are
+								matriculating to an accredited 2-year or 4-year college this
+								Fall are eligible to apply.
 							</p>
 							<p className={cn('text-2xl text-gray-800 font-semibold')}>
-								Heading 2
+								Scholarship Awards
 							</p>
-							{[1, 2, 3, 4].map(() => (
-								<p>
-									What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-									printing and typesetting industry. Lorem Ipsum has been the
-									industry's standard dummy text ever since the 1500s, when an
-									unknown printer took a galley of type and scrambled it to make
-									a type specimen book. It has survived not only five centuries,
-									but also the leap into electronic typesetting, remaining
-									essentially unchanged. It was popularised in the 1960s with
-									the release of Letraset sheets containing Lorem Ipsum
-									passages, and more recently with desktop publishing software
-									like Aldus PageMaker including versions of Lorem Ipsum
-								</p>
-							))}
 							<p>
-								It has survived not only five centuries, but also the leap into
-								electronic typesetting, remaining essentially unchanged.{' '}
-								<span className={cn('font-bold text-gray-800')}>
-									Lorem Ipsum is simply dummy text of the printing and
-									typesetting industry
+								<span className='font-medium text-sm underline'>
+									Academic Achievement Award
+								</span>
+								<br />
+								This scholarship honors students with outstanding academic
+								records and commitment to learning. Recipients excel
+								consistently and engage in rigorous coursework and innovative
+								research.{' '}
+								<span className='italic'>
+									Applicant's GPA must be in the{' '}
+									<span className='font-medium'>top 15%</span> of his/her class
+									to apply.
 								</span>
 							</p>
-							<p>Your future of your higher education journey starts here.</p>
+							<p>
+								<span className='font-medium text-sm underline'>
+									Outstanding Student-Athlete Award
+								</span>
+								<br />
+								This award honors students who excel academically and
+								athletically. Recipients balance competitive spirit with
+								scholastic diligence and demonstrate leadership under pressure.
+								<span className='italic'>
+									Applicants must have at minimum{' '}
+									<span className='font-medium'>two years</span> of varsity
+									athletic experience to apply.
+								</span>
+							</p>
+							<p>
+								<span className='font-medium text-sm underline'>
+									Community Leadership Award
+								</span>
+								<br />
+								This scholarship celebrates individuals who positively impact
+								their communities through service and leadership. Applicants
+								must show meaningful involvement and proactive civic engagement.
+							</p>
+							<p>
+								<span className='font-medium text-sm underline'>
+									Entrepreneurial Excellence Award
+								</span>
+								<br />
+								This award honors students who drive innovation and turn ideas
+								into impact. Candidates must show a forward-thinking mindset and
+								seize opportunities for growth.
+							</p>
+							<p>The future of your higher education journey starts here.</p>
 						</div>
 						<div className='mb-6 lg:w-1/2'>
 							<p className={cn('text-2xl text-gray-800 font-semibold')}>FAQ</p>
 							{[
 								{
-									question: 'Question 1',
+									question: 'Can I apply for more than one scholarship?',
 									answer:
-										"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+										'Yes, we recommend that you apply to any of the scholarships that you believe you are a good fit for. However, students can only be awarded at most one scholarship.',
 								},
 								{
-									question: 'Question 2',
+									question: 'When is the deadline to apply?',
 									answer: '',
 									answersPrefix:
-										'Lorem Ipsum is simply dummy text of the printing and typesetting industry:',
+										'The following are the most important dates for the scholarship:',
 									answers: [
-										"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-										'An unknown printer took a galley of type and scrambled it to make a type specimen book',
-										'It was popularised in the 1960s with the release of Letraset',
+										'The priority application window is open until 11:59pm EST on February 20th.',
+										'Late applications will be accepted until our interview process ends March 23rd.',
 									],
+								},
+								{
+									question: 'When will I know if I have been selected?',
+									answer: 'Final decisions will be delivered in mid-April.',
 								},
 							].map(
 								({ question, answer, answers = [], answersPrefix = '' }) => {
@@ -275,3 +321,12 @@ export const getStaticProps: GetStaticProps<PageStaticProps> = () => {
 		props: ROUTE_STATIC_PROPS,
 	});
 };
+
+function isApplicationSubmitted(
+	applicationForLoggedInUser: ScholarshipApplication | null,
+): applicationForLoggedInUser is ScholarshipApplication {
+	return (
+		applicationForLoggedInUser != null &&
+		applicationForLoggedInUser.description === 'submitted'
+	);
+}
