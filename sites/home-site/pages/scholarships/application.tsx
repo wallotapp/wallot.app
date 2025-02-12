e44d6224655cc9e47b1c91f0127cc32606a1a0d8
Kamar <kamarmack.home@gmail.com>
2025-02-12 18:01:06 +0000

    Close #264

---
+++ sites/home-site/pages/scholarships/application.tsx
@@ -0,0 +1,60 @@
+import type { GetStaticProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import {
+	PageStaticProps,
+	PageProps,
+	Page as PageComponent,
+} from 'ergonomic-react/src/components/nextjs-pages/Page';
+import { HomeSiteRouteQueryParams } from '@wallot/js';
+
+const Page: NextPage<PageProps> = (props) => {
+	// ==== Hooks ==== //
+
+	// Router
+	const router = useRouter();
+
+	// ==== Constants ==== //
+
+	// Router Query
+	const query: RouteQueryParams = router?.query ?? {};
+
+	// Router Query Param Values
+	const _ = query;
+	typeof _;
+	// Runtime Route ID
+	const ROUTE_RUNTIME_ID = props.routeStaticId;
+
+	// Runtime Page Props
+	const pageProps: PageProps = {
+		...props,
+		routeId: ROUTE_RUNTIME_ID,
+	};
+
+	// ==== Render ==== //
+	return (
+		<PageComponent {...pageProps}>
+			<p>Application</p>
+		</PageComponent>
+	);
+};
+
+export default Page;
+
+// ==== Static Page Props ==== //
+
+// Route Static ID
+const ROUTE_STATIC_ID = 'HOME_SITE__/SCHOLARSHIPS/APPLICATION' as const;
+
+// Route Query Params Type
+type RouteQueryParams = HomeSiteRouteQueryParams[typeof ROUTE_STATIC_ID];
+
+export const getStaticProps: GetStaticProps<PageStaticProps> = () => {
+	// Route Static Props
+	const ROUTE_STATIC_PROPS: PageStaticProps = {
+		routeStaticId: ROUTE_STATIC_ID,
+		title: 'Florida Visionary Scholarship Application',
+	};
+	return Promise.resolve({
+		props: ROUTE_STATIC_PROPS,
+	});
+};
