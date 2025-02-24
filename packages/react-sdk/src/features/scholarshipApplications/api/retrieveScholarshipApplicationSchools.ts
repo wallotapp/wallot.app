56c996737e292e62f113903c54b737d4eaf82586
Kamar <kamarmack.home@gmail.com>
2025-02-24 01:48:43 +0000

    Fix retrieveScholarshipApplicationSchools API request implementation

--- packages/react-sdk/src/features/scholarshipApplications/api/retrieveScholarshipApplicationSchools.ts
+++ packages/react-sdk/src/features/scholarshipApplications/api/retrieveScholarshipApplicationSchools.ts
@@ -1,7 +1,7 @@
+import { default as ky } from 'ky-universal';
 import { handleKyError } from 'ergonomic';
 import { School } from '@wallot/js';
 import { DEPLOYMENT_ENV } from 'ergonomic-react/src/config/deploymentEnv';
-import { defaultKyInstance } from '@wallot/react/src/lib/ky';
 
 export async function retrieveScholarshipApplicationSchools() {
 	try {
@@ -18,8 +18,8 @@ export async function retrieveScholarshipApplicationSchools() {
 			);
 		}
 
-		const data = await defaultKyInstance
-			.get(scholarshipApplicationSchoolsDownloadUrl)
+		const data = await ky
+			.get(scholarshipApplicationSchoolsDownloadUrl, { mode: 'no-cors' })
 			.json<School[]>();
 		return data;
 	} catch (err) {
