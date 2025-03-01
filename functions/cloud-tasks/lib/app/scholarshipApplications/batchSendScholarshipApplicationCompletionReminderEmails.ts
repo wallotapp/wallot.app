9cfa0f1d45f53e29caa860487ec6ac8a43875244
Kamar <kamarmack.home@gmail.com>
2025-03-01 10:30:53 +0000

    Draft batch reminder logic

--- functions/cloud-tasks/lib/app/scholarshipApplications/batchSendScholarshipApplicationCompletionReminderEmails.ts
+++ functions/cloud-tasks/lib/app/scholarshipApplications/batchSendScholarshipApplicationCompletionReminderEmails.ts
@@ -1,5 +1,12 @@
 import { CloudTaskHandler, firebaseFunctions } from 'ergonomic-node';
-import { gmail, log } from '../../services.js';
+import {
+	ScholarshipApplication,
+	UserActivatedByAlpaca,
+	scholarshipApplicationsApi,
+	usersApi,
+} from '@wallot/js';
+import { db, gmail, log } from '../../services.js';
+import { SendEmailWithGmailAPIParams } from '@wallot/node';
 
 export const handleBatchSendScholarshipApplicationCompletionReminderEmailsTaskOptions =
 	{
@@ -14,12 +21,35 @@ export const handleBatchSendScholarshipApplicationCompletionReminderEmailsTask: 
 	BatchSendScholarshipApplicationCompletionReminderEmailsParams
 > = async ({ data: { ceiling } }) => {
 	try {
-		// Pre-scale
+		// More simple
 		// Query all the scholarship applications
 		// Query all the users
+		const [scholarshipApplicationDocs, userDocs] = await Promise.all([
+			db.collection(scholarshipApplicationsApi.collectionId).get(),
+			db.collection(usersApi.collectionId).get(),
+		]);
+		const scholarshipApplications = scholarshipApplicationDocs.docs.map(
+			(doc) => doc.data() as ScholarshipApplication,
+		);
+		const users = userDocs.docs.map(
+			(doc) => doc.data() as UserActivatedByAlpaca,
+		);
 		// Locate the relevant scholarship applications and users in-memory via filter() and find()
+		const scholarshipApplicationsToEmail = scholarshipApplications.filter(
+			(application) =>
+				application.status === 'in_progress' &&
+				(application.reminder_emails_sent_for_application_completion == null ||
+					application.reminder_emails_sent_for_application_completion <
+						ceiling),
+		);
 		// Construct the Gmail API parameters from the user's email address and first name
+		const gmailParamSet = scholarshipApplicationsToEmail.map(
+			(application): SendEmailWithGmailAPIParams => {
+				return {};
+			},
+		);
 		// Send the emails 8 seconds apart
+		// Increment each `reminder_emails_sent_for_application_completion` property by 1 via a batch update
 		const result = await gmail.sendEmail();
 		log({
 			message:
@@ -47,8 +77,9 @@ export const handleBatchSendScholarshipApplicationCompletionReminderEmailsTask: 
 	}
 };
 
-// At-scale
+// More scalable
 // Query all the scholarship applications whose status is `in_progress` and whose `reminder_emails_sent_for_application_completion` property is an integer less than `ceiling`
 // Query each scholarship application's underlying user by reading the `scholarshipApplication.user` foreign key property and executing chunks of Firestore array-contains-any queries over the user document's `_id` property
 // Construct the Gmail API parameters from the user's email address and first name
 // Send the emails 8 seconds apart
+// Increment each `reminder_emails_sent_for_application_completion` property by 1 via a batch update
