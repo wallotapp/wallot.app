294d7017e7637dae61fcc38e54c589d58782651d
Kamar <kamarmack.home@gmail.com>
2025-03-17 00:22:56 +0000

    refactor research application form data properties to improve schema structure and validation

--- packages/javascript-sdk/scholarshipApplications/utils/researchApplicationFormDataProperties.ts
+++ packages/javascript-sdk/scholarshipApplications/utils/researchApplicationFormDataProperties.ts
@@ -1,4 +1,4 @@
-import { EnumMember, GeneralizedFieldTypeEnum, getEnum } from 'ergonomic';
+import { GeneralizedFieldTypeEnum, YupHelpers } from 'ergonomic';
 import * as yup from 'yup';
 
 export type ResearchApplicationFormSchema = {
@@ -26,106 +26,84 @@ export const fallbackResearchApplicationFormSchema: ResearchApplicationFormSchem
 export type ResearchApplicationServerData = Record<string, never>;
 export type ResearchApplicationClientData = Record<string, never>;
 
+const checkbox = () =>
+	yup
+		.mixed()
+		.oneOf([true, false, null, ''])
+		.default(null)
+		.optional()
+		.meta({ type: GeneralizedFieldTypeEnum.obj.boolean });
+const list = () =>
+	YupHelpers.array(
+		yup.object({
+			title: yup.string().default('').defined(),
+			details: yup.string().default('').defined(),
+		}),
+	).defined();
+
 export const researchApplicationFormDataPropertiesBySectionSource = {
-	0: {
-		research_application_s0_q0: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.long_text,
-		}),
-		research_application_s0_q1: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.long_text,
-		}),
-		research_application_s0_q2: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.short_text,
-		}),
-		research_application_s0_q3: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.long_text,
-		}),
-	},
-	2: {
-		research_application_s2_q0: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.long_text,
-		}),
-		research_application_s2_q1: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.short_text,
-		}),
-		research_application_s2_q2: yup.string().default('').optional().meta({
-			type: GeneralizedFieldTypeEnum.obj.long_text,
-		}),
-		research_application_s2_q3: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.long_text,
-		}),
-		research_application_s2_q5: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.short_text,
-		}),
-		research_application_s2_q4: yup.string().default('').optional().meta({
-			type: GeneralizedFieldTypeEnum.obj.long_text,
-		}),
-	},
-	3: {
-		research_application_s3_q0: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.short_text,
-		}),
-		research_application_s3_q1: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.short_text,
-		}),
-		research_application_s3_q2: yup.string().default('').optional().meta({
-			type: GeneralizedFieldTypeEnum.obj.long_text,
-		}),
-	},
-	4: {
-		research_application_s4_q0: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.short_text,
-		}),
-		research_application_s4_q1: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.short_text,
-		}),
-		research_application_s4_q4: yup.string().default('').optional().meta({
-			type: GeneralizedFieldTypeEnum.obj.short_text,
-		}),
-		research_application_s4_q5: yup.string().default('').optional().meta({
-			type: GeneralizedFieldTypeEnum.obj.short_text,
-		}),
-	},
-	5: {
-		research_application_s5_q0: yup.string().default('').required().meta({
-			type: GeneralizedFieldTypeEnum.obj.long_text,
-		}),
-	},
+	research_application_s0_q0: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.long_text,
+	}),
+	research_application_s0_q1: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.long_text,
+	}),
+	research_application_s0_q2: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.short_text,
+	}),
+	research_application_s0_q3: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.long_text,
+	}),
+	research_application_s1_q0: YupHelpers.array(
+		yup.string().defined(),
+	).defined(),
+	research_application_s2_q0: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.long_text,
+	}),
+	research_application_s2_q1: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.short_text,
+	}),
+	research_application_s2_q2: yup.string().default('').optional().meta({
+		type: GeneralizedFieldTypeEnum.obj.long_text,
+	}),
+	research_application_s2_q3: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.long_text,
+	}),
+	research_application_s2_q5: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.short_text,
+	}),
+	research_application_s2_q4: yup.string().default('').optional().meta({
+		type: GeneralizedFieldTypeEnum.obj.long_text,
+	}),
+	research_application_s3_q0: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.short_text,
+	}),
+	research_application_s3_q1: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.short_text,
+	}),
+	research_application_s3_q2: yup.string().default('').optional().meta({
+		type: GeneralizedFieldTypeEnum.obj.long_text,
+	}),
+	research_application_s4_q0: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.short_text,
+	}),
+	research_application_s4_q1: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.short_text,
+	}),
+	research_application_s4_q2: list(),
+	research_application_s4_q3: list(),
+	research_application_s4_q4: yup.string().default('').optional().meta({
+		type: GeneralizedFieldTypeEnum.obj.short_text,
+	}),
+	research_application_s4_q5: yup.string().default('').optional().meta({
+		type: GeneralizedFieldTypeEnum.obj.short_text,
+	}),
+	research_application_s5_q0: yup.string().default('').required().meta({
+		type: GeneralizedFieldTypeEnum.obj.long_text,
+	}),
+	research_application_s6_q0: checkbox(),
+	research_application_s6_q1: checkbox(),
+	research_application_s6_q2: checkbox(),
+	research_application_s6_q3: checkbox(),
+	research_application_s6_q4: checkbox(),
 };
-
-export const ResearchApplicationFormDataFieldKeyEnum = getEnum([
-	'research_application_s0_q0',
-	'research_application_s0_q1',
-	'research_application_s0_q2',
-	'research_application_s0_q3',
-	'research_application_s1_q0',
-	'research_application_s2_q0',
-	'research_application_s2_q1',
-	'research_application_s2_q2',
-	'research_application_s2_q3',
-	'research_application_s2_q4',
-	'research_application_s2_q5',
-	'research_application_s3_q0',
-	'research_application_s3_q1',
-	'research_application_s3_q2',
-	'research_application_s4_q0',
-	'research_application_s4_q1',
-	'research_application_s4_q2',
-	'research_application_s4_q3',
-	'research_application_s4_q4',
-	'research_application_s4_q5',
-	'research_application_s5_q0',
-	'research_application_s6_q0',
-	'research_application_s6_q1',
-	'research_application_s6_q2',
-	'research_application_s6_q3',
-	'research_application_s6_q4',
-]);
-export type ResearchApplicationFormDataFieldKey = EnumMember<
-	typeof ResearchApplicationFormDataFieldKeyEnum
->;
-
-export type ResearchApplicationFormData = Record<
-	ResearchApplicationFormDataFieldKey,
-	string
->;
