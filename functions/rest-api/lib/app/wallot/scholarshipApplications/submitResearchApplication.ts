634aebfd7fde311b0c781418bf198ae345df7a74
Kamar <kamarmack.home@gmail.com>
2025-03-17 06:02:53 +0000

    add functions to save and submit research applications with permission validation

---
+++ functions/rest-api/lib/app/wallot/scholarshipApplications/submitResearchApplication.ts
@@ -0,0 +1,70 @@
+import { type DecodedIdToken as FirebaseUser } from 'firebase-admin/auth';
+import { FunctionResponse } from '@wallot/node';
+import {
+	ResearchApplicationFormDataRouteParams,
+	ResearchApplicationFormDataResponse,
+	ResearchApplicationFormDataParams,
+	scholarshipApplicationsApi,
+	ScholarshipApplication,
+} from '@wallot/js';
+import { secrets } from '../../../secrets.js';
+import { db, gmail } from '../../../services.js';
+
+export const submitResearchApplication = async (
+	params: ResearchApplicationFormDataParams,
+	{ scholarshipApplicationId }: ResearchApplicationFormDataRouteParams,
+	_query: Record<string, never>,
+	firebaseUser: FirebaseUser | null,
+): Promise<FunctionResponse<ResearchApplicationFormDataResponse>> => {
+	if (!firebaseUser) throw new Error('Unauthorized');
+
+	const researchApplicationRef = db
+		.collection(scholarshipApplicationsApi.collectionId)
+		.doc(scholarshipApplicationId);
+
+	// Query application
+	const researchApplicationDoc = await researchApplicationRef.get();
+	if (!researchApplicationDoc.exists)
+		throw new Error('Invalid scholarshipApplicationId');
+	const { user: userId } =
+		researchApplicationDoc.data() as ScholarshipApplication;
+
+	// Validate permissions
+	if (firebaseUser.uid !== userId)
+		throw new Error(
+			"Modifying another user's scholarship application is not allowed",
+		);
+
+	// Submit application
+	const updateParams: ResearchApplicationFormDataParams = { ...params };
+	await researchApplicationRef.update(updateParams);
+
+	const onFinished = async () => {
+		if (
+			secrets.SECRET_CRED_SERVER_PROTOCOL === 'https' &&
+			secrets.SECRET_CRED_DEPLOYMENT_ENVIRONMENT == 'live'
+		) {
+			// Send developer alert
+			const alertBody = JSON.stringify(
+				{
+					user_id: firebaseUser.uid,
+					scholarship_application_id: scholarshipApplicationId,
+					...params,
+				},
+				null,
+				2,
+			);
+			await gmail.sendDeveloperAlert({
+				message: `New research application submission from ${
+					firebaseUser.email ?? firebaseUser.uid
+				}.\
+<br/><br/>\
+${alertBody}`,
+				subject:
+					'[Wallot Developer Alerts] New Research Application Submission',
+			});
+		}
+	};
+
+	return { onFinished, json: {} };
+};
